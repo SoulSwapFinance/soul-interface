2c909bc0dabe09d0a576133ddbae9a90d3ac6e90
Buns <bunsthedev@gmail.com>
2021-07-28 18:21:40 -0500

    chore: naming

--- src/features/swap/AdvancedSwapDetails.tsx
+++ src/features/swap/AdvancedSwapDetails.tsx
@@ -89,9 +89,9 @@ export function AdvancedSwapDetails({ trade, allowedSlippage, minerBribe }: Adva
 
       <RowBetween>
         <RowFixed>
-          <div className="text-sm text-secondary">{i18n._(t`xSUSHI Fee`)}</div>
+          <div className="text-sm text-secondary">{i18n._(t`SPELL Fee`)}</div>
           <QuestionHelper
-            text={i18n._(t`A portion of each trade (0.05%) goes to xSUSHI holders as a protocol incentive.`)}
+            text={i18n._(t`A portion of each trade (0.05%) goes to SPELL holders as a protocol incentive.`)}
           />
         </RowFixed>
         <div className="text-sm font-bold text-high-emphesis">
