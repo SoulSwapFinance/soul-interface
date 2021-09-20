3494480d7ad2ee950444a45f0633c5b99fa0bc8f
lucus.eth <lucusra@protonmail.com>
2021-09-21 02:04:58 +1000

    add: stake | stake rows!

---
+++ src/features/stake/StakePids.js
@@ -0,0 +1,20 @@
+import { tokens } from '../farm/tokens'
+
+// Base tokens (token1):
+// - WFTM
+// - FUSD
+// - SOUL
+
+export const SummonerPid0 = [
+  {
+    pid: 0,
+    token1: "SOUL",
+    token2: "SOUL",
+    lpSymbol: "SOUL/SOUL",
+    lpAddresses: {
+      4002: "0xCF174A6793FA36A73e8fF18A71bd81C985ef5aB5",
+    },
+    token1Address: tokens.SOUL,
+    token2Address: tokens.SOUL,
+  },
+];
---
+++ src/pages/stake/index.jsx
@@ -0,0 +1,25 @@
+import { Wrap } from '../../components/ReusableStyles'
+
+import { ChainId } from '@soulswap/sdk'
+import Container from '../../components/Container'
+import Head from 'next/head'
+import React from 'react'
+
+import { StakeList } from '../../features/stake/StakePairRow'
+
+const Farm = () => {
+  return (
+    <Wrap padding="4rem 0 0 0" justifyContent="center">
+      <Container id="farm-page">
+        <Head>
+          <title>Stake | Soul</title>
+          <meta key="description" name="description" content="Stake SOUL" />
+        </Head>
+
+        <StakeList />
+      </Container>
+    </Wrap>
+  )
+}
+
+export default Farm
