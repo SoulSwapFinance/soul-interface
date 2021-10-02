228391ec5481676e3b25ed9fae56822dda19b67e
Buns <bunsthedev@gmail.com>
2021-10-01 23:17:51 -0500

    update: constants

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -7,6 +7,47 @@ export const TIMELOCK_ADDRESS = '0x1a9C8182C09F50C8318d769245beA52c32BE35BC'
 export const ARCHER_ROUTER_ADDRESS: AddressMap = {
   [ChainId.MAINNET]: '0x9917C083FF9FbD29Df1367FBF7F2388A9a202431',
 }
+export const FACTORY_ADDRESS = {
+  [ChainId.MAINNET]: '',
+  [ChainId.FANTOM]: '0x1120e150dA9def6Fe930f4fEDeD18ef57c0CA7eF',
+  [ChainId.FANTOM_TESTNET]: '',
+}
+
+export const SOUL_VAULT_ADDRESS: AddressMap = {
+  [ChainId.MAINNET]: '',
+  [ChainId.FANTOM]: '',
+  [ChainId.FANTOM_TESTNET]: '',
+}
+
+export const LOCKER_ADDRESS: AddressMap = {
+  [ChainId.MAINNET]: '',
+  [ChainId.FANTOM]: '',
+  [ChainId.FANTOM_TESTNET]: '',
+}
+
+export const SOUL_SEANCE_PAIR: AddressMap = {
+  [ChainId.MAINNET]: '',
+  [ChainId.FANTOM]: '0x8f1E15cD3d5a0bb85B8189d5c6B61BB64398E19b', // 01 OCT
+  [ChainId.FANTOM_TESTNET]: '',
+}
+
+export const SEANCE_USDC_PAIR: AddressMap = {
+  [ChainId.MAINNET]: '',
+  [ChainId.FANTOM]: '0x98c678d3c7ebed4a36b84666700d8b5b5ddc1f79', // 01 OCT
+  [ChainId.FANTOM_TESTNET]: '',
+}
+
+export const ETH_USD_PAIR: AddressMap = {
+  [ChainId.MAINNET]: '',
+  [ChainId.FANTOM]: '0x160653F02b6597E7Db00BA8cA826cf43D2f39556', // FTM-USDC
+  [ChainId.FANTOM_TESTNET]: '',
+}
+
+export const SOUL_USDC_PAIR: AddressMap = {
+  [ChainId.MAINNET]: '',
+  [ChainId.FANTOM]: '', // FTM-USDC
+  [ChainId.FANTOM_TESTNET]: '',
+}
 
 export const SOUL_SUMMONER_ADDRESS: AddressMap = {
   // [ChainId.MAINNET]: '',
@@ -30,23 +71,4 @@ export const MULTICALL2_ADDRESS: AddressMap = {
   // [ChainId.FANTOM]: '0xEd2Fb478f7fCef33E1E1d980a0135789B295a7F5', // 29 AUG
   [ChainId.FANTOM]: '0xf682Cc4468608fC4eFbaD6a06D9BC72e7790075a', // 29 AUG
   [ChainId.FANTOM_TESTNET]: '0x1ACB479bB9D1F73009F85ef5F495E942Bb57f15A', // 7 JUL
-  // [ChainId.ROPSTEN]: '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696',
-  // [ChainId.RINKEBY]: '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696',
-  // [ChainId.GÖRLI]: '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696',
-  // [ChainId.KOVAN]: '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696',
-  // [ChainId.ARBITRUM]: '0xadF885960B47eA2CD9B55E6DAc6B42b7Cb2806dB',
-  // [ChainId.ARBITRUM_TESTNET]: '0xa501c031958F579dB7676fF1CE78AD305794d579',
-  // [ChainId.CELO]: '0x9aac9048fC8139667D6a2597B902865bfdc225d3',
-  // [ChainId.MATIC]: '0x02817C1e3543c2d908a590F5dB6bc97f933dB4BD',
-  // [ChainId.MATIC_TESTNET]: '',
-  // [ChainId.XDAI]: '0x67dA5f2FfaDDfF067AB9d5F025F8810634d84287',
-  // [ChainId.MOONBEAM_TESTNET]: '',
-  // [ChainId.AVALANCHE]: '0xdDCbf776dF3dE60163066A5ddDF2277cB445E0F3',
-  // [ChainId.AVALANCHE_TESTNET]: '',
-  // [ChainId.HECO]: '0xdDCbf776dF3dE60163066A5ddDF2277cB445E0F3',
-  // [ChainId.HECO_TESTNET]: '',
-  // [ChainId.HARMONY]: '0xdDCbf776dF3dE60163066A5ddDF2277cB445E0F3',
-  // [ChainId.HARMONY_TESTNET]: '',
-  // [ChainId.OKEX]: '0xF4d73326C13a4Fc5FD7A064217e12780e9Bd62c3',
-  // [ChainId.OKEX_TESTNET]: '',
 }
