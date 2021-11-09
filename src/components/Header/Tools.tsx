c4522171d287b44208017167243472b5c43b1083
Buns <bunsthedev@gmail.com>
2021-11-09 16:16:15 -0600

    update: Header

--- src/components/Header/Tools.tsx
+++ src/components/Header/Tools.tsx
@@ -105,7 +105,7 @@ export default function Menu() {
                           </a>
                         </NavLink>
                         ) : ''
-                      )
+                      // )
                   )}
                 </div>
               </div>
