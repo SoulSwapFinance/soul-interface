556578558351a59b980640d3e0918b171c3cc06a
lucus.eth <lucusra@protonmail.com>
2021-11-10 19:32:07 +1100

    update: Farm | UI upgrade

--- src/features/farm/FarmsList.jsx
+++ src/features/farm/FarmsList.jsx
@@ -4,9 +4,10 @@ import { ethers } from 'ethers'
 import useActiveWeb3React from '../../hooks/useActiveWeb3React'
 import useSoulSummoner from './hooks/useSoulSummoner'
 
+import FarmKey from './FarmKey'
 import FarmRowRender from './FarmRowRender'
 import { FarmPids } from './FarmPids'
-import { Wrap, Heading, Text, Button } from '../../components/ReusableStyles' // Heading, Text
+import { Wrap, Heading, Text, Button } from './ReusableStyles' // Heading, Text
 
 const FarmList = () => {
   const { chainId, account } = useActiveWeb3React()
@@ -58,27 +59,8 @@ const FarmList = () => {
 
   return (
     <>
-      <Wrap padding="0 0 2rem 0">
-        {/* <Heading fontSize="1.5rem" textAlign="center">
-          Farms
-        </Heading> */}
-        {/* <Text fontSize="1rem" padding="0" color="#aaa" textAlign="center">
-          Each farm withdraw, no matter how small, resets the fee percentage.
-        </Text> */}
-
-        {/* <Text fontSize=".9rem" padding="1rem 0 0 0" color="#F36FFE" textAlign="center">
-          Total Pending Rewards: { 
-          Number(totalPending)
-          .toFixed(0)
-          .toString()
-          .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-          }
-        </Text> */}
-        {/* <Wrap display='flex' justifyContent='center'>
-          <Button onClick={() => handleHarvest()}>Harvest Rewards</Button>
-        </Wrap> */}
-      </Wrap>
-      <div>{farmList}</div>
+      <FarmKey />
+      <>{farmList}</>
     </>
   )
 }
