bf7f5374cdd8a645568c3440244dca31d299c0c3
Buns <bunsthedev@gmail.com>
2021-11-12 02:55:12 -0600

    add: CloverWallet, WalletModal, delete: connectors

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -1,13 +1,14 @@
-import { Web3Provider } from '@ethersproject/providers'
 import { BscConnector } from '@binance-chain/bsc-connector'
 // import { ChainId } from '../sdk'
-import { FortmaticConnector } from './Fortmatic'
+import { FortmaticConnector } from '../entities/FortmaticConnector'
 import { InjectedConnector } from '@web3-react/injected-connector'
-import { NetworkConnector } from './NetworkConnector'
+import { NetworkConnector } from '../entities/NetworkConnector'
 import { PortisConnector } from '@web3-react/portis-connector'
 import { TorusConnector } from '@web3-react/torus-connector'
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 import { WalletLinkConnector } from '@web3-react/walletlink-connector'
+import { Web3Provider } from '@ethersproject/providers'
+import { CloverConnector } from '@clover-network/clover-connector'
 
 enum ChainId { // TODO
   MAINNET = 1,
@@ -18,7 +19,6 @@ enum ChainId { // TODO
 
 export const RPC = {
   [ChainId.MAINNET]: 'https://eth-mainnet.alchemyapi.io/v2/q1gSNoSMEzJms47Qn93f9-9Xg5clkmEC',
-  // [ChainId.MAINNET]: 'https://eth-mainnet.alchemyapi.io/v2/nj9dkG3QULVVIs_hIMq26wL0ZwV4dJ3x',
   [ChainId.BSC]: 'https://bsc-dataseed.binance.org/',
   [ChainId.FANTOM]: 'https://rpc.ftm.tools',
   // [ChainId.FANTOM_TESTNET]: 'https://rpc.testnet.fantom.network',
@@ -26,9 +26,13 @@ export const RPC = {
   // [ChainId.RINKEBY]: 'https://eth-rinkeby.alchemyapi.io/v2/XVLwDlhGP6ApBXFz_lfv0aZ6VmurWhYD',
   // [ChainId.GÖRLI]: 'https://eth-goerli.alchemyapi.io/v2/Dkk5d02QjttYEoGmhZnJG37rKt8Yl3Im',
   // [ChainId.KOVAN]: 'https://eth-kovan.alchemyapi.io/v2/6OVAa_B_rypWWl9HqtiYK26IRxXiYqER',
+  // [ChainId.FANTOM]: 'https://rpcapi.fantom.network',
   // [ChainId.MATIC]: 'https://rpc-mainnet.maticvigil.com',
+  // [ChainId.MATIC]:
+  //     'https://apis.ankr.com/e22bfa5f5a124b9aa1f911b742f6adfe/c06bb163c3c2a10a4028959f4d82836d/polygon/full/main',
   // [ChainId.MATIC_TESTNET]: 'https://rpc-mumbai.matic.today',
   // [ChainId.XDAI]: 'https://rpc.xdaichain.com',
+  // [ChainId.BSC_TESTNET]: 'https://data-seed-prebsc-2-s3.binance.org:8545',
   // [ChainId.MOONBEAM_TESTNET]: 'https://rpc.testnet.moonbeam.network',
   // [ChainId.AVALANCHE]: 'https://api.avax.network/ext/bc/C/rpc',
   // [ChainId.AVALANCHE_TESTNET]: 'https://api.avax-test.network/ext/bc/C/rpc',
@@ -39,6 +43,14 @@ export const RPC = {
   // [ChainId.OKEX]: 'https://exchainrpc.okex.org',
   // [ChainId.OKEX_TESTNET]: 'https://exchaintestrpc.okex.org',
   // [ChainId.ARBITRUM]: 'https://arb1.arbitrum.io/rpc',
+  // [ChainId.PALM]: 'https://palm-mainnet.infura.io/v3/da5fbfafcca14b109e2665290681e267',
+}
+
+export function getNetwork(defaultChainId, urls = RPC) {
+  return new NetworkConnector({
+    defaultChainId,
+    urls,
+  })
 }
 
 export const network = new NetworkConnector({
@@ -52,60 +64,37 @@ export function getNetworkLibrary(): Web3Provider {
   return (networkLibrary = networkLibrary ?? new Web3Provider(network.provider as any))
 }
 
-export const injected = new InjectedConnector({
-  supportedChainIds: [
-    // 1, // mainnet
-    // 56, // bsc
-    250, // fantom
-    // 4002, // fantom testnet
-    // 3, // ropsten
-    // 4, // rinkeby
-    // 5, // goreli
-    // 42, // kovan
-    // 137, // matic
-    // 80001, // matic testnet
-    // 100, // xdai
-    // 1287, // moonbase
-    // 43114, // avalanche
-    // 43113, // fuji
-    // 128, // heco
-    // 256, // heco testnet
-    // 1666600000, // harmony
-    // 1666700000, // harmony testnet
-    // 66, // okex testnet
-    // 65, // okex testnet
-    // 42161, // arbitrum
-    // 42220, // celo
-  ],
-})
+const supportedChainIds = [
+  // 1, // mainnet
+  // 56, // binance smart chain
+  250, // fantom
+  // 4002, // fantom testnet
+  // 3, // ropsten
+  // 4, // rinkeby
+  // 5, // goreli
+  // 42, // kovan
+  // 137, // matic
+  // 80001, // matic testnet
+  // 100, // xdai
+  // 97, // binance smart chain testnet
+  // 1287, // moonbase
+  // 43114, // avalanche
+  // 43113, // fuji
+  // 128, // heco
+  // 256, // heco testnet
+  // 1666600000, // harmony
+  // 1666700000, // harmony testnet
+  // 66, // okex testnet
+  // 65, // okex testnet
+  // 42161, // arbitrum
+  // 42220, // celo
+  // 11297108109, // palm
+  // 1285, // moonriver
+]
 
-export const bridgeInjected = new InjectedConnector({
-  supportedChainIds: [
-    1, // mainnet
-    // 3, // ropsten
-    // 4, // rinkeby
-    // 5, // goreli
-    // 42, // kovan
-    250, // fantom
-    // 4002, // fantom testnet
-    // 137, // matic
-    // 80001, // matic testnet
-    // 100, // xdai
-    56, // binance smart chain
-    // 97, // binance smart chain testnet
-    // 1287, // moonbase
-    // 1285, // moonriver
-    // 43114, // avalanche
-    // 43113, // fuji
-    // 128, // heco
-    // 256, // heco testnet
-    // 1666600000, // harmony
-    // 1666700000, // harmony testnet
-    // 66, // okex testnet
-    // 65, // okex testnet
-    // 42161, // arbitrum
-    // 42220, // celo
-  ],
+
+export const injected = new InjectedConnector({
+  supportedChainIds,
 })
 
 // mainnet only
@@ -113,16 +102,15 @@ export const walletconnect = new WalletConnectConnector({
   rpc: { [ChainId.FANTOM]: RPC[ChainId.FANTOM] },
   bridge: 'https://bridge.walletconnect.org',
   qrcode: true,
+  supportedChainIds,
   pollingInterval: 15000,
 })
 
-// // mainnet only
-export const fortmatic = new FortmaticConnector(
-  {
+// mainnet only
+export const fortmatic = new FortmaticConnector({
   apiKey: process.env.NEXT_PUBLIC_FORTMATIC_API_KEY ?? '',
-  chainId: 1, 
-  }
-)
+  chainId: 1,
+})
 
 // mainnet only
 export const portis = new PortisConnector({
@@ -135,7 +123,7 @@ export const walletlink = new WalletLinkConnector({
   url: RPC[ChainId.MAINNET],
   appName: 'SoulSwap',
   appLogoUrl:
-    'https://raw.github.com/SoulSwapFinance/icons/master/token/soul.jpg',
+  'https://raw.github.com/SoulSwapFinance/icons/master/token/soul.jpg',
 })
 
 // mainnet only
@@ -145,3 +133,7 @@ export const torus = new TorusConnector({
 
 // binance only
 export const binance = new BscConnector({ supportedChainIds: [56] })
+
+export const clover = new CloverConnector({
+  supportedChainIds: [250]
+})
