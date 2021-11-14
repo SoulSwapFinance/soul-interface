a27f89aa8a8fd3b42ddf493f5653ece968bbec0a
Buns <bunsthedev@gmail.com>
2021-11-13 22:52:30 -0600

    create: Charts, and Explore pages

---
+++ src/pages/charts/index.jsx
@@ -0,0 +1,13 @@
+const Charts = () => {
+	return (
+      <iframe 
+			frameBorder={"none"}
+    		title={"INFO"}
+    		src="https://info.soulswap.finance"
+    		height={"100%" }
+    		width={"100%"}
+    />
+	);
+};
+
+export default Charts;
