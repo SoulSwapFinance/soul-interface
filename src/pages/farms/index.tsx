be9a4ccb7a46cb199ff67d059145d66ad444c074
Buns <bunsthedev@gmail.com>
2021-11-16 21:42:06 -0600

    add: Stables page

---
+++ src/pages/farms/index.tsx
@@ -0,0 +1,85 @@
+import { ArrowRightIcon } from '@heroicons/react/outline'
+import Container from '../../components/Container'
+import Head from 'next/head'
+import { I18n } from '@lingui/core'
+import Link from 'next/link'
+import Typography from '../../components/Typography'
+import { t } from '@lingui/macro'
+import { useLingui } from '@lingui/react'
+import { useMemo } from 'react'
+
+const FARMS = (i18n: I18n) => [
+  {
+    id: 1,
+    name: 'SOUL PAIRS',
+    description: i18n._(t`Deposit your SOUL-LP to earn $SOUL rewards.`),
+    href: '/farm',
+  },
+  {
+    id: 2,
+    name: 'SEANCE PAIRS',
+    description: 'Borrow $SEANCE with $SOUL and earn rewards.',
+    href: './seance'
+  },
+  {
+    id: 3,
+    name: 'FANTOM PAIRS',
+    description: 'Deposit $SEANCE to earn $WFTM and $UNIDX.',
+    href: './circles'
+  },
+  {
+    id: 4,
+    name: 'STABLE PAIRS',
+    description: 'Deposit $SEANCE and earn protocol fee-shares.',
+    href: './enchant'
+  },
+  {
+    id: 5,
+    name: 'WETH PAIRS',
+    description: 'Send $SOUL for a small sacrifice of $SEANCE.',
+    href: './scarab/create'
+  },
+  {
+    id: 6,
+    name: 'BTC PAIRS',
+    description: 'Bridge from Binance, Ethereum, Huobi, and more.',
+    href: 'https://bridge.soul.sh'
+  },
+]
+
+export default function Tools() {
+  const { i18n } = useLingui()
+  const features = useMemo(() => FARMS(i18n), [i18n])
+
+  return (
+    <Container id="features-page" className="py-4 space-y-4 md:py-8 lg:py-12" maxWidth="xl">
+      <Head>
+        <title>Explore | Soul</title>
+        <meta key="description" name="description" content="SoulSwap Tools..." />
+      </Head>
+      <Typography variant="h1" component="h1">
+        OUR ECOSYSTEM
+      </Typography>
+      <ul className="space-y-4 divide-y-0">
+        {features.map((feature) => (
+          <li key={feature.id} className="relative w-full p-4 rounded bg-dark-900 hover:bg-dark-800">
+            <div className="flex justify-between space-x-4">
+              <div className="flex-1 min-w-0">
+                <Link href={feature.href}>
+                  <a className="flex items-center justify-between focus:outline-none">
+                    <span className="absolute inset-0" aria-hidden="true" />
+                    <div className="space-y-1">
+                      <p className="text-xl font-bold truncate text-primary">{feature.name}</p>
+                      <p className="text-sm truncate text-secondary">{feature.description}</p>
+                    </div>
+                    <ArrowRightIcon width={24} height={24} className="text-high-emphesis" />
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </Container>
+  )
+}
---
+++ src/pages/stables/index.jsx
@@ -0,0 +1,27 @@
+import { Wrap } from '../../components/ReusableStyles'
+import DoubleGlowShadowV2 from '../../components/DoubleGlowShadowV2'
+import Container from '../../components/Container'
+import Head from 'next/head'
+import React from 'react'
+
+import StablesList from '../../features/farm/StablesList'
+
+const Stables = () => {
+  return (
+    <Wrap padding='2rem 0 0 0' justifyContent="center">
+      <DoubleGlowShadowV2 opacity="0.6">
+
+      <Container id="farm-page">
+        <Head>
+          <title>Farm with Stables ($SOUL)</title>
+          <meta key="description" name="description" content="Farm SOUL" />
+        </Head>
+        <StablesList />
+      </Container>
+      </DoubleGlowShadowV2>
+
+    </Wrap>
+  )
+}
+
+export default Stables
