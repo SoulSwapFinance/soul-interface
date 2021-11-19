371e4f0a35238c664aa2d15082c46feb3c048ccc
Buns <bunsthedev@gmail.com>
2021-11-19 03:27:04 -0600

    update: farms ordering

--- src/features/farm/Pids.ts
+++ src/features/farm/Pids.ts
@@ -38,20 +38,6 @@ export const AllPids = [
   },
 
   {
-    // 1200
-    pid: 24,
-    token1: 'ENCHANT',
-    token2: 'FTM',
-    lpSymbol: 'ENCHANT-FTM',
-    lpAddresses: {
-      4002: '',
-      250: '0xb4d6ff768f409e4d102bad80f9a8ac105453ccdd',
-    },
-    token1Address: tokens.ENCHANT,
-    token2Address: tokens.FTM,
-  },
-
-  {
     // 600
     pid: 22,
     token1: 'SOUL',
@@ -123,19 +109,6 @@ export const AllPids = [
 
   {
     // 400
-    pid: 29,
-    token1: 'ENCHANT',
-    token2: 'USDC',
-    lpSymbol: 'ENCHANT-USDC',
-    lpAddresses: {
-      4002: '',
-      250: '0x6c6f32008262666CA4acEd0a824c4945AB96e5F3',
-    },
-    token1Address: tokens.ENCHANT,
-    token2Address: tokens.USDC,
-  },
-  {
-    // 400
     pid: 27,
     token1: 'SEANCE',
     token2: 'UNIDX',
@@ -173,6 +146,35 @@ export const AllPids = [
     token1Address: tokens.SEANCE,
     token2Address: tokens.USDC,
   },
+
+  {
+    // 1200
+    pid: 24,
+    token1: 'ENCHANT',
+    token2: 'FTM',
+    lpSymbol: 'ENCHANT-FTM',
+    lpAddresses: {
+      4002: '',
+      250: '0xb4d6ff768f409e4d102bad80f9a8ac105453ccdd',
+    },
+    token1Address: tokens.ENCHANT,
+    token2Address: tokens.FTM,
+  },
+
+  {
+    // 400
+    pid: 29,
+    token1: 'ENCHANT',
+    token2: 'USDC',
+    lpSymbol: 'ENCHANT-USDC',
+    lpAddresses: {
+      4002: '',
+      250: '0x6c6f32008262666CA4acEd0a824c4945AB96e5F3',
+    },
+    token1Address: tokens.ENCHANT,
+    token2Address: tokens.USDC,
+  },
+
   {
     // 100
     pid: 11,
@@ -238,19 +240,7 @@ export const AllPids = [
     token1Address: tokens.FTM,
     token2Address: tokens.MIM,
   },
-  {
-    // 400
-    pid: 17,
-    token1: 'BNB',
-    token2: 'DAI',
-    lpSymbol: 'BNB-DAI',
-    lpAddresses: {
-      4002: '',
-      250: '0xC1EdFbA9811B696bDFd07d31eD5FF702e031364E',
-    },
-    token1Address: tokens.BNB,
-    token2Address: tokens.DAI,
-  },
+
   {
     // 600
     pid: 18,
@@ -277,32 +267,7 @@ export const AllPids = [
     token1Address: tokens.FTM,
     token2Address: tokens.WETH,
   },
-  {
-    // 600
-    pid: 8,
-    token1: 'WETH',
-    token2: 'BTC',
-    lpSymbol: 'ETH-BTC',
-    lpAddresses: {
-      4002: '',
-      250: '0x1fc954d3484bc21e0ce53a6648a35bbfc03dc9d0',
-    },
-    token1Address: tokens.WETH,
-    token2Address: tokens.WBTC,
-  },
-  {
-    // 600
-    pid: 20,
-    token1: 'BTC',
-    token2: 'USDC',
-    lpSymbol: 'BTC-USDC',
-    lpAddresses: {
-      4002: '',
-      250: '0xE2d39F7f0c8517Ca2058Fa97eB3D8d4928F7C6D6',
-    },
-    token1Address: tokens.WBTC,
-    token2Address: tokens.USDC,
-  },
+  
   {
     // 600
     pid: 13,
@@ -343,30 +308,43 @@ export const AllPids = [
     token2Address: tokens.USDC,
   },
   {
-    // 200
-    pid: 21,
-    token1: 'USDC',
-    token2: 'DAI',
-    lpSymbol: 'USDC-DAI',
+    // 600
+    pid: 8,
+    token1: 'WETH',
+    token2: 'BTC',
+    lpSymbol: 'ETH-BTC',
     lpAddresses: {
       4002: '',
-      250: '0x406de3a93f6b4179e3b21a3d81226b43e1918fd9',
+      250: '0x1fc954d3484bc21e0ce53a6648a35bbfc03dc9d0',
     },
-    token1Address: tokens.USDC,
-    token2Address: tokens.DAI,
+    token1Address: tokens.WETH,
+    token2Address: tokens.WBTC,
   },
   {
-    // 200
-    pid: 16,
-    token1: 'DAI',
-    token2: 'gFUSDT',
-    lpSymbol: 'DAI-gFUSDT',
+    // 600
+    pid: 20,
+    token1: 'BTC',
+    token2: 'USDC',
+    lpSymbol: 'BTC-USDC',
     lpAddresses: {
       4002: '',
-      250: '0xe637D90A993EDBD75AC09E9fcB16313D193B9451',
+      250: '0xE2d39F7f0c8517Ca2058Fa97eB3D8d4928F7C6D6',
     },
-    token1Address: tokens.DAI,
-    token2Address: tokens.gFUSDT,
+    token1Address: tokens.WBTC,
+    token2Address: tokens.USDC,
+  },
+  {
+    // 400
+    pid: 17,
+    token1: 'BNB',
+    token2: 'DAI',
+    lpSymbol: 'BNB-DAI',
+    lpAddresses: {
+      4002: '',
+      250: '0xC1EdFbA9811B696bDFd07d31eD5FF702e031364E',
+    },
+    token1Address: tokens.BNB,
+    token2Address: tokens.DAI,
   },
   {
     // 200
@@ -394,6 +372,33 @@ export const AllPids = [
     token1Address: tokens.USDC,
     token2Address: tokens.FUSD,
   },
+  {
+    // 200
+    pid: 21,
+    token1: 'USDC',
+    token2: 'DAI',
+    lpSymbol: 'USDC-DAI',
+    lpAddresses: {
+      4002: '',
+      250: '0x406de3a93f6b4179e3b21a3d81226b43e1918fd9',
+    },
+    token1Address: tokens.USDC,
+    token2Address: tokens.DAI,
+  },
+  {
+    // 200
+    pid: 16,
+    token1: 'DAI',
+    token2: 'gFUSDT',
+    lpSymbol: 'DAI-gFUSDT',
+    lpAddresses: {
+      4002: '',
+      250: '0xe637D90A993EDBD75AC09E9fcB16313D193B9451',
+    },
+    token1Address: tokens.DAI,
+    token2Address: tokens.gFUSDT,
+  },
+  
   {
     // 200
     pid: 30,
