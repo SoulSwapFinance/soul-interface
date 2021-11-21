65aa4659c8eac42a4a6be64934c7365a6e575eff
Buns <bunsthedev@gmail.com>
2021-11-21 00:34:34 -0600

    update: swap headers + add: bridge

---
+++ src/pages/bridge/index.tsx
@@ -0,0 +1,31 @@
+import SwapHeader from '../../features/trade/Header'
+import DoubleGlowShadowV2 from '../../components/DoubleGlowShadowV2'
+import Container from '../../components/Container'
+// import { useIsSwapUnsupported } from '../../hooks/useIsSwapUnsupported'
+
+// const addIsUnsupported = useIsSwapUnsupported(currencies?.CURRENCY_A, currencies?.CURRENCY_B)
+
+const Bridge = () => {
+	return (
+    <DoubleGlowShadowV2 opacity="0.6">
+        <Container id="charts-page" maxWidth="2xl" className="space-y-4">
+          <div className="p-4 space-y-4 rounded bg-dark-900" style={{ zIndex: 1 }}>
+            <SwapHeader
+              // input={currencies[Field.CURRENCY_A]}
+              // output={currencies[Field.CURRENCY_B]}
+              // allowedSlippage={allowedSlippage}
+            />     
+            <iframe 
+				frameBorder={"none"}
+				title={"BRIDGE"}
+				src="https://bridge.soul.sh"
+              height={'720' }
+              width={"100%"}
+            />
+	        </div>
+        </Container>
+      </DoubleGlowShadowV2>
+
+)}
+
+export default Bridge
