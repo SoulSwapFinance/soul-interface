201356360a33da671ffd3170ddcef56794f61744
Buns <bunsthedev@gmail.com>
2021-11-30 05:32:53 -0600

    add: farm banner for gFUSDT discontinue

--- src/pages/farms/index.tsx
+++ src/pages/farms/index.tsx
@@ -1,5 +1,6 @@
 import { Wrap } from '../../components/ReusableStyles'
 import DoubleGlowShadowV2 from '../../components/DoubleGlowShadowV2'
+import FarmBanner from '../../components/FarmBanner'
 import Container from '../../components/Container'
 import Head from 'next/head'
 
@@ -12,6 +13,8 @@ const Farms = () => {
     <Wrap padding='1rem 0 0 0' justifyContent="center">
       <DoubleGlowShadowV2 opacity="0.6">
       <Container id="farm-page">
+        <FarmBanner/>
+        <br/>
         <Head>
           <title>Farm | All</title>
           <meta key="description" name="description" content="Farm SOUL" />
