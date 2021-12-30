8e91d2f7d80a841aad274354e2ca1ee372aca8ec
Buns <bunsthedev@gmail.com>
2021-12-30 02:44:24 -0600

    mobile-friendly: token stats

--- src/components/TokenStats/index.tsx
+++ src/components/TokenStats/index.tsx
@@ -20,7 +20,7 @@ function TokenStats(): JSX.Element | null {
       className="flex items-center rounded bg-dark-900 hover:bg-dark-800 p-0.5 whitespace-nowrap text-sm font-bold cursor-pointer select-none pointer-events-auto"
       onClick={() => toggleTokenStatsModal()} // todo
     >
-      <div className="grid items-center grid-flow-col px-3 py-2 space-x-2 text-sm rounded-lg pointer-events-auto auto-cols-max bg-dark-1000 text-secondary">
+      <div className="grid items-center grid-flow-col px-0 py-2 space-x-2 text-sm rounded-lg pointer-events-auto auto-cols-max bg-dark-1000 text-secondary">
         {/* <Image src={NETWORK_ICON[chainId]} alt="Switch Network" className="rounded-md" width="22px" height="22px" /> */}
         <Image
             src="/images/tokens/soul.png"
@@ -30,9 +30,8 @@ function TokenStats(): JSX.Element | null {
             objectFit="contain"
             className="rounded-md"
         />        
-      <div className="text-primary">{'SOUL'}</div>
+      {/* <div className="text-primary">{'SOUL'}</div> */}
       </div>
-      {/* <NetworkModel /> */}
       <TokenStatsModal />
     </div>
   )
