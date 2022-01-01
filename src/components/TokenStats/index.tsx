1204f8297f9dfc8922e584d89db640e0d9a4c6e1
Buns <bunsthedev@gmail.com>
2022-01-01 11:36:22 -0600

    add to header: SEANCE

--- src/components/TokenStats/index.tsx
+++ src/components/TokenStats/index.tsx
@@ -12,7 +12,7 @@ import { usePriceHelperContract } from '../../features/bond/hooks/useContract'
 import styled from 'styled-components'
 
 const HideOnMobile = styled.div`
-@media screen and (max-width: 550px) {
+@media screen and (max-width: 500px) {
   display: none;
 }
 `;
@@ -22,6 +22,7 @@ function TokenStats(): JSX.Element | null {
   const { chainId } = useActiveWeb3React()
   const toggleTokenStatsModal = useToggleTokenStatsModal()
   const open = useModalOpen(ApplicationModal.SOUL_STATS)
+
   const priceHelperContract = usePriceHelperContract()
 
   const rawSoulPrice = useSingleCallResult(priceHelperContract, 'currentTokenUsdcPrice', ['0xe2fb177009FF39F52C0134E8007FA0e4BaAcBd07'])?.result
@@ -33,42 +34,63 @@ function TokenStats(): JSX.Element | null {
   console.log(Number(rawLuxPrice))
   const luxPrice = formatCurrency(Number(rawLuxPrice) / 1E18, 2)
   console.log(luxPrice)
-  
+
+  const rawSeancePrice = useSingleCallResult(priceHelperContract, 'currentTokenUsdcPrice', ['0x124B06C5ce47De7A6e9EFDA71a946717130079E6'])?.result
+  console.log(Number(rawSeancePrice))
+  const seancePrice = formatCurrency(Number(rawSeancePrice) / 1E18, 3)
+  console.log(seancePrice)
+
   if (!chainId) return null
-  
+
   return (
     <div
-      className="flex items-center rounded bg-dark-900 hover:bg-dark-800 p-0.5 whitespace-nowrap text-sm font-bold cursor-pointer select-none pointer-events-auto"
-      onClick={() => toggleTokenStatsModal()} // todo
+      className="flex items-center md:space-x-2 rounded bg-dark-900 hover:bg-dark-800 p-0.5 whitespace-nowrap text-sm font-bold cursor-pointer select-none pointer-events-auto"
+      onClick={() => toggleTokenStatsModal()}
     >
+      <div className="grid items-center grid-flow-col px-1.5 py-1 space-x-2 text-sm rounded-lg pointer-events-auto auto-cols-max bg-dark-1000 text-secondary">
+        <Image
+          src="/images/tokens/lux.png"
+          alt="LUX"
+          width="28px"
+          height="28px"
+          objectFit="contain"
+          className="rounded-md"
+        />
+        {/* <HideOnMobile> */}
         <HideOnMobile>
-      <div className="grid items-center grid-flow-col px-1 py-1 space-x-2 text-sm rounded-lg pointer-events-auto auto-cols-max bg-dark-1000 text-secondary">
-        {/* <Image src={NETWORK_ICON[chainId]} alt="Switch Network" className="rounded-md" width="22px" height="22px" /> */}
-        <Image
-            src="/images/tokens/LUXOR.svg"
-            alt="LUX"
-            width="25px"
-            height="25px"
-            objectFit="contain"
-            className="rounded-md"
-            />
-        <div className="text-primary">{luxPrice}</div>
-        </div>
-        </HideOnMobile>
-      
-      <div className="grid items-center grid-flow-col px-1 py-1 space-x-2 text-sm rounded-lg pointer-events-auto auto-cols-max bg-dark-1000 text-secondary">
-        <Image
-            src="/images/tokens/soul.png"
-            alt="SOUL"
-            width="25px"
-            height="25px"
-            objectFit="contain"
-            className="rounded-md"
-        />   
-        <HideOnMobile>     
-          <div className="text-primary">{soulPrice}</div>
+          <div className="text-primary">{luxPrice}</div>
         </HideOnMobile>
       </div>
+
+      <div className="grid items-center grid-flow-col px-1.5 py-1 space-x-2 text-sm rounded-lg pointer-events-auto auto-cols-max bg-dark-1000 text-secondary">
+        <Image
+          src="/images/tokens/seance.png"
+          alt="SEANCE"
+          width="28px"
+          height="28px"
+          objectFit="contain"
+          className="rounded-md"
+        />
+        <HideOnMobile>
+          <div className="text-primary">{seancePrice}</div>
+        </HideOnMobile>
+      </div>
+
+      <div className="grid items-center grid-flow-col px-1.5 py-1 space-x-2 text-sm rounded-lg pointer-events-auto auto-cols-max bg-dark-1000 text-secondary">
+        <Image
+          src="/images/tokens/soul.png"
+          alt="SOUL"
+          width="28px"
+          height="28px"
+          objectFit="contain"
+          className="rounded-md"
+        />
+        {/* <HideOnMobile> */}
+          <div className="text-primary">{soulPrice}</div>
+        {/* </HideOnMobile> */}
+      </div>
+
+
       <TokenStatsModal />
     </div>
   )
