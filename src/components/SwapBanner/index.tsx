935d248196223c1617bec075efef46af5ae76111
Buns <bunsthedev@gmail.com>
2022-01-09 17:16:38 -0600

    fix: smol errors

--- src/components/SwapBanner/index.tsx
+++ src/components/SwapBanner/index.tsx
@@ -1,5 +1,6 @@
-import { FC } from 'react'
+import React, { FC } from 'react'
 import { XIcon } from '@heroicons/react/outline'
+import NavLink from 'components/NavLink'
 
 const SwapBanner: FC = () => (
   <div className="relative w-full bg-purple bg-opacity-10">
@@ -7,30 +8,25 @@ const SwapBanner: FC = () => (
       <div className="pr-16 sm:text-center sm:px-16">
         <p className="font-medium text-white">
           <span className="centered md:hidden">Harvest Claims - Now Available. 
-          {/* <br/> Use Filter: Retired Vaults */}
                     </span>
           <span className="hidden md:inline">Harvest Claims - Now Available.
-          {/* <br/> Use Filter: Retired Vaults */}
           </span>
           <br/>
           <span className="block sm:ml-2 md:inline-block">
-            <a href="/claims" target = "_blank" rel="noreferrer"
+            <NavLink src="/claims" target = "_blank" rel="noreferrer"
               className="font-bold text-white underline">
               {' '}
-              {/* <br /> */}
               Click Here to Claim<span aria-hidden="true"></span>
-            </a>
+            </NavLink>
           </span>
         </p>
       </div>
       <div className="absolute inset-y-0 right-0 flex items-start pt-1 pr-1 sm:pt-1 sm:pr-2 sm:items-start">
-        {/*
+        
         <button type="button" className="flex p-2 focus:outline-none">
-           <span className="sr-only">Dismiss</span> 
-           */}
-           
-          {/* <XIcon className="w-6 h-6 text-white" aria-hidden="true" /> */}
-        {/* </button> */}
+           <span className="sr-only">Dismiss</span>
+        <XIcon className="w-6 h-6 text-white" aria-hidden="true" />
+        </button>
       </div>
     </div>
   </div>
