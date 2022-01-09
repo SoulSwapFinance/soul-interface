b223a65338817f763ffce0d2d0cc1d92536fb038
Buns <bunsthedev@gmail.com>
2022-01-09 16:52:03 -0600

    init: APR as SOUL Annually

--- src/features/mines/MineListItem.tsx
+++ src/features/mines/MineListItem.tsx
@@ -6,33 +6,21 @@ import DoubleLogo from 'components/DoubleLogo'
 import QuestionHelper from 'components/QuestionHelper'
 import { classNames, formatNumber, formatPercent } from 'functions'
 import { useCurrency } from 'hooks/Tokens'
-import { Interface } from '@ethersproject/abi'
-import ISoulSwapPair from 'constants/abis/soulswap/ISoulSwapPair.json'
-import useSoulMine from './hooks/useSoulMine'
-import { PairType } from './enum'
 import MineListItemDetails from './MineListItemDetails'
-import { SOUL, SOUL_SUMMONER_ADDRESS } from '../../constants'
-import { useActiveWeb3React,
+import { SOUL } from '../../constants'
+import {
+  useActiveWeb3React,
   useHarvestHelperContract,
-   usePriceHelperContract,
-  useSoulSummonerContract } from 'hooks'
-import Logo from 'components/Logo'
+  usePriceHelperContract
+} from 'hooks'
 import { useTVL } from 'hooks/useV2Pairs'
 import { useMultipleContractSingleData, useSingleCallResult } from 'state/multicall/hooks'
 import { POOLS } from 'constants/farms'
-import { Token } from 'sdk'
-import { getAddress } from '@ethersproject/address'
-// import LiquidityPrice from 'components/Liquidity/LiquidityPrice'
 import { useV2PairsWithPrice } from 'hooks/useV2Pairs'
-
-// import useTokenBalance from 'hooks/useTokenBalance'
-// import { useSoulFarms } from './hooks'
-
-// const PAIR_INTERFACE = new Interface(ISoulSwapPair)
 
 const MineListItem = ({ farm, ...rest }) => {
   const { chainId } = useActiveWeb3React()
-  
+
   const token0 = useCurrency(farm.pair.token0?.id)
   const token1 = useCurrency(farm.pair.token1?.id)
   const priceHelperContract = usePriceHelperContract()
@@ -42,17 +30,14 @@ const MineListItem = ({ farm, ...rest }) => {
   console.log(Number(rawSoulPrice))
   const soulPrice = Number(rawSoulPrice) / 1E18
   console.log(soulPrice)
-  
+
   const tvlInfo = useTVL()
 
   let [data] = useV2PairsWithPrice([[token0, token1]])
   let [state, pair, pairPrice] = data
-  
+
   const lpBalance = useSingleCallResult(harvestHelperContract, 'fetchBals', [farm?.id])?.result
-  // const lpPrice = useSingleCallResult(harvestHelperContract, 'fetchBals', [farm?.id])?.result
-  // const lpPrice = useSingleCallResult(priceHelperContract, 'currentTokenUsdcPrice', [farm?.id])?.result
   console.log('lpBalance: %s', Number(lpBalance))
-  // const pidTvl = useTVL()
 
   let tvl = tvlInfo.map((previousValue, currentValue) => {
     return previousValue.tvl + currentValue
@@ -61,70 +46,119 @@ const MineListItem = ({ farm, ...rest }) => {
   let summTvl = tvlInfo.reduce((previousValue, currentValue) => {
     return previousValue + currentValue.tvl
   }, 0)
-  
+
   // let lpPrice = tvlInfo.reduce((previousValue, currentValue) => {
   //   return previousValue + currentValue.lpPrice
   // }, 0)
-  
+
   const farmingPools = Object.keys(POOLS[chainId]).map((key) => {
     return { ...POOLS[chainId][key], lpToken: key }
   })
+
+  // const roiPerSecond =
+  // farm?.rewards?.reduce((previousValue, currentValue) => {
+  //   return previousValue + currentValue.rewardPerBlock * currentValue.rewardPrice
+  // }, 0) / summTvl
+
+  const roiPerSecond =
+    farm?.rewards?.reduce((previousValue, currentValue) => {
+      return previousValue + currentValue.rewardPerBlock * currentValue.rewardPrice
+    }, 0) / summTvl
+
+
+  const secsPerHour = 60 * 60
+
+  const rewardAprPerHour = roiPerSecond * secsPerHour
+  const rewardAprPerDay = rewardAprPerHour * 24
+  const rewardAprPerMonth = rewardAprPerDay * 30
+  const rewardAprPerYear = rewardAprPerMonth * 12
+
+  const roiPerHour = rewardAprPerHour
+  const roiPerMonth = rewardAprPerMonth
+  const roiPerDay = rewardAprPerDay
+  const roiPerYear = rewardAprPerYear
+
+
+  // const roiPerSecond = 1E1000
+  // const roiPerHour = roiPerSecond * 60 * 60
+  // const roiPerDay = roiPerHour * 24
+  // const roiPerMonth = roiPerDay * 30
+  // const roiPerYear = roiPerDay * 365
 
   return (
     <Disclosure {...rest}>
       {({ open }) => (
         <div>
-        { token1 ? 
-          <Disclosure.Button
-          className={classNames(
-            open && 'rounded-b-none',
-            'w-full px-4 py-6 text-left rounded cursor-pointer select-none bg-dark-900 text-primary text-sm md:text-lg'
-            )}
+          {token1 ?
+            <Disclosure.Button
+              className={classNames(
+                open && 'rounded-b-none',
+                'w-full px-4 py-6 text-left rounded cursor-pointer select-none bg-dark-900 text-primary text-sm md:text-lg'
+              )}
             >
-            <div className="grid grid-cols-4">
-              <div className="flex col-span-2 space-x-4 md:col-span-1">
-              { token1 ?
-                <DoubleLogo currency0={ token0 } currency1={ token1 } size={40} />
-                : <CurrencyLogo currency={ token0 } size={54}/> 
-              }
-                <div className="flex flex-col justify-center">
-                  <div>
-                    <span className="font-bold">{farm?.pair?.token0?.symbol}</span>
+              <div className="grid grid-cols-4">
+                <div className="flex col-span-2 space-x-4 md:col-span-1">
+                  {token1 ?
+                    <DoubleLogo currency0={token0} currency1={token1} size={40} />
+                    : <CurrencyLogo currency={token0} size={54} />
+                  }
+                  <div className="flex flex-col justify-center">
+                    <div>
+                      <span className="font-bold">{farm?.pair?.token0?.symbol}</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="flex flex-col justify-center font-bold">{
-                formatNumber(
-                 // PRICE PER TOKEN * TOKEN BALANCE
-                  Number(pairPrice) * Number(lpBalance) / 1e18,
-                  true)
-              }</div>
-              {/* <div className="flex flex-col justify-center font-bold">{formatNumber(farm.tvl, true)}</div> */}
-              <div className="flex-row items-center hidden space-x-4 md:flex">
-                <div className="flex items-center space-x-2">
-                  {farm?.rewards?.map((reward, i) => (
-                    <div key={i} className="flex items-center">
-                      <CurrencyLogo currency={SOUL[chainId]} size={isMobile ? 32 : 50} />
-                    </div>
-                  ))}
-                </div>
-                <div className="flex flex-col space-y-1">
-                  {farm?.rewards?.map((reward, i) => (
-                    <div key={i} className="text-xs md:text-sm whitespace-nowrap">
-                      {reward.rewardPerDay > 0 ?
-                        formatNumber(reward.rewardPerDay) + ' / DAY'
-                        : 'ZERO'
+                <div className="flex flex-col justify-center font-bold">{
+                  formatNumber(
+                    // PRICE PER TOKEN * TOKEN BALANCE
+                    Number(pairPrice) * Number(lpBalance) / 1e18,
+                    true)
+                }</div>
+                <div className="flex-row items-center hidden space-x-4 md:flex">
+                  <div className="flex items-center space-x-2">
+                    {farm?.rewards?.map((reward, i) => (
+                      <div key={i} className="flex items-center">
+                        <CurrencyLogo currency={SOUL[chainId]} size={isMobile ? 32 : 50} />
+                      </div>
+                    ))}
+                  </div>
+                  <div className="flex flex-col space-y-1">
+                    {farm?.rewards?.map((reward, i) => (
+                      <div key={i} className="text-xs md:text-sm whitespace-nowrap">
+                        {reward.rewardPerDay > 0 ?
+                          formatNumber(reward.rewardPerDay) + ' / DAY'
+                          : 'ZERO'
                         }
-                    </div>
-                  ))}
+                      </div>
+                    ))}
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-col items-end justify-center">
-                <div className="flex flex-row items-center font-bold text-right text-high-emphesis">
+                <div className="flex flex-col items-end justify-center">
+                  <div className="flex flex-row items-center font-bold text-right text-high-emphesis">
+
+                    {farm?.rewards?.map((reward, i) => (
+                      <div key={i} className="text-xs md:text-sm whitespace-nowrap">
+                        {
+                          reward.rewardPerDay > 0 
+                          ? formatNumber(
+                            (reward?.rewardPerDay) // rewards per year (annualized)
+                            * (365)
+                            // divided by TVL
+                            // / 1e18
+                            )
+                          : 'ZERO'
+                        }
+                      </div>
+                    ))}
+
+                  </div>
+
+
+                  {/* <div className="flex flex-row items-center font-bold text-right text-high-emphesis">
                   {farm?.tvl !== 0
                     ? farm?.roiPerYear > 10000
                       ? '>10,000%'
-                      : formatPercent(farm.roiPerYear * 100)
+                      : formatPercent(farm?.roiPerYear * 100)
                     : 'Infinite'}
                   {!!farm?.feeApyPerYear && (
                     <QuestionHelper
@@ -146,12 +180,13 @@ const MineListItem = ({ farm, ...rest }) => {
                       }
                     />
                   )}
+                </div> */}
+
+                  <div className="text-xs text-right md:text-base text-secondary">annualized</div>
                 </div>
-                <div className="text-xs text-right md:text-base text-secondary">annualized</div>
               </div>
-            </div>
-          </Disclosure.Button>
-          : '' }
+            </Disclosure.Button>
+            : ''}
           {open && <MineListItemDetails farm={farm} />}
         </div>
       )}
