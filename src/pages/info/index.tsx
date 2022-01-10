a8f6f5cae8a409b2c37a29b2d4a377dd6a108abe
Buns <bunsthedev@gmail.com>
2022-01-09 20:55:25 -0600

    increase: Chart page size

--- src/pages/info/index.tsx
+++ src/pages/info/index.tsx
@@ -1,15 +1,13 @@
 import SwapHeader from '../../features/trade/Header'
 import DoubleGlowShadowV2 from '../../components/DoubleGlowShadowV2'
 import Container from '../../components/Container'
-// import { useIsSwapUnsupported } from '../../hooks/useIsSwapUnsupported'
-
-// const addIsUnsupported = useIsSwapUnsupported(currencies?.CURRENCY_A, currencies?.CURRENCY_B)
+import { Wrap } from '../../components/ReusableStyles'
 
 const Info = () => {
 	return (
     <DoubleGlowShadowV2 opacity="0.6">
-        <Container id="charts-page" maxWidth="2xl" className="space-y-4">
-          <div className="p-4 space-y-4 rounded bg-dark-900" style={{ zIndex: 1 }}>
+      <Container id="charts-page" maxWidth="7xl" className="grid h-full grid-flow-col grid-cols-5 mx-auto gap-9">
+        <div className="col-span-6 space-y-6 bg-dark-900 lg:col-span-6" style={{ zIndex: 1 }}>
             <SwapHeader />     
             <iframe 
               frameBorder={"none"}
