a0e4b00c27335191fa9708cded808332962ef018
Buns <bunsthedev@gmail.com>
2022-01-13 13:56:43 -0600

    add: create page

--- src/layouts/Kashi/index.tsx
+++ src/layouts/Kashi/index.tsx
@@ -10,6 +10,7 @@ import React from 'react'
 // import kashiLogo from '../../../public/kashi-logo.png'
 // import kashiLogo from '../../../public/kashi-logo.png'
 import { useRouter } from 'next/router'
+import { formatNumber } from 'functions'
 
 interface LayoutProps {
   left?: JSX.Element
