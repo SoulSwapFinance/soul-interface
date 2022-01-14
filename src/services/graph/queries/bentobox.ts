ee1b3522ecbd470c3c68e35a4aef1bf92ecf566d
Buns <bunsthedev@gmail.com>
2022-01-13 18:32:07 -0600

    update / add: graph fetchers

--- src/services/graph/queries/bentobox.ts
+++ src/services/graph/queries/bentobox.ts
@@ -1,5 +1,38 @@
 import gql from 'graphql-tag'
 
+export const cloneFieldsQuery = gql`
+  fragment cloneFields on Clone {
+    id
+    address: id
+    data
+    block
+    timestamp
+  }
+`
+
+export const clonesQuery = gql`
+  query clones(
+    $skip: Int = 0
+    $first: Int = 1000
+    $where: Clone_filter
+    $block: Block_height
+    $orderBy: Clone_orderBy
+    $orderDirection: OrderDirection
+  ) {
+    clones(
+      skip: $skip
+      first: $first
+      where: $where
+      block: $block
+      orderBy: $orderBy
+      orderDirection: $orderDirection
+    ) {
+      ...cloneFields
+    }
+  }
+  ${cloneFieldsQuery}
+`
+
 export const bentoTokenFieldsQuery = gql`
   fragment bentoTokenFields on Token {
     id
@@ -12,6 +45,27 @@ export const bentoTokenFieldsQuery = gql`
     block
     timestamp
   }
+`
+
+export const bentoTokensQuery = gql`
+  query bentoTokens($first: Int = 1000, $block: Block_height, $where: Token_filter) {
+    tokens(first: $first, skip: $skip, block: $block, where: $where) {
+      ...bentoTokenFields
+    }
+  }
+  ${bentoTokenFieldsQuery}
+`
+
+export const bentoUserTokensQuery = gql`
+  query bentoUserTokens($user: String!, $skip: Int = 0, $first: Int = 1000, $block: Block_height) {
+    userTokens(skip: $skip, first: $first, block: $block, where: { share_gt: 0, user: $user }) {
+      token {
+        ...bentoTokenFields
+      }
+      share
+    }
+  }
+  ${bentoTokenFieldsQuery}
 `
 
 export const kashiPairFieldsQuery = gql`
@@ -73,4 +127,66 @@ export const kashiPairsQuery = gql`
     }
   }
   ${kashiPairFieldsQuery}
+`
+
+export const kashiUserPairsQuery = gql`
+  query kashiUserPairs($user: String!, $skip: Int = 0, $first: Int = 1000, $block: Block_height) {
+    userKashiPairs(skip: $skip, first: $first, block: $block, where: { user: $user }) {
+      assetFraction
+      collateralShare
+      borrowPart
+      pair {
+        ...kashiPairFields
+      }
+    }
+  }
+  ${kashiPairFieldsQuery}
+`
+
+export const bentoBoxQuery = gql`
+  query bentoBoxQuery(
+    $id: String! = "0xf5bce5077908a1b7370b9ae04adc565ebd643966"
+    $block: Block_height
+    $where: BentoBox_filter
+  ) {
+    bentoBoxes(first: 1, block: $block, where: $where) {
+      id
+      totalUsers
+      totalTokens
+      totalKashiPairs
+      tokens(first: 1000) {
+        id
+        name
+        symbol
+        decimals
+        totalSupplyBase
+        totalSupplyElastic
+      }
+    }
+  }
+`
+
+export const bentoStrategiesQuery = gql`
+  query bentoStrategies($first: Int = 1000, $firstHarvests: Int = 3, $block: Block_height, $where: Strategy_filter!) {
+    strategies(first: $first, block: $block, where: $where) {
+      token {
+        id
+        decimals
+        strategyTargetPercentage
+        totalSupplyElastic
+      }
+      balance
+      totalProfit
+      harvests(first: $firstHarvests, orderBy: timestamp, orderDirection: desc) {
+        id
+        profit
+        tokenElastic
+        timestamp
+        block
+      }
+      timestamp
+      block
+    }
+  }
+  ${bentoTokenFieldsQuery}
 `
