88a90bd04c395d9510844258d05051acd0270fcb
Buns <bunsthedev@gmail.com>
2022-01-27 01:30:28 -0600

    add [hooks]: usePositions, usePairPrice

---
+++ src/hooks/usePositions.ts
@@ -0,0 +1,43 @@
+import { useMemo } from 'react'
+import { Contract } from '@ethersproject/contracts'
+import { useActiveWeb3React } from "services/web3"
+import { useSingleCallResult, useSingleContractMultipleData } from "state/multicall/hooks"
+import zip from 'lodash/zip'
+import { Zero } from '@ethersproject/constants'
+import { useSoulSummonerContract } from 'hooks'
+
+export function useSoulPositions(contract?: Contract | null) {
+    const { account } = useActiveWeb3React()
+  
+    const numberOfPools = useSingleCallResult(contract ? contract : null, 'poolLength')?.result
+  
+    const args = useMemo(() => {
+      if (!account || !numberOfPools) {
+        return
+      }
+      // return [...Array(numberOfPools).keys()].map((pid) => [String(pid), String(account)])
+      return [...Array(Number(numberOfPools)).keys()].map((pid) => [String(pid), String(account)])
+    }, [numberOfPools, account])
+  
+    const pendingSoul = useSingleContractMultipleData(args ? contract : null, 'pendingSoul', args)
+    const userInfo = useSingleContractMultipleData(args ? contract : null, 'userInfo', args)
+  
+    return useMemo(() => {
+      if (!pendingSoul || !userInfo) {
+        return []
+      }
+      return zip(pendingSoul, userInfo)
+        .map((data, i) => ({
+          id: args[i][0],
+          pendingSoul: data[0].result?.[0] || Zero,
+          amount: data[1].result?.[0] || Zero,
+        }))
+        .filter(({ pendingSoul, amount }) => {
+          return (pendingSoul && !pendingSoul.isZero()) || (amount && !amount.isZero())
+        })
+    }, [args, pendingSoul, userInfo])
+  }
+  
+export function usePositions() {
+    return useSoulPositions(useSoulSummonerContract())
+  }
--- src/hooks/usePrice.ts
+++ src/hooks/usePrice.ts
@@ -1,6 +1,8 @@
 import { usePriceHelperContract } from "hooks"
 import { useMemo } from "react"
-import { useSingleCallResult } from "state/multicall/hooks"
+import { NEVER_RELOAD, useSingleCallResult } from "state/multicall/hooks"
+import { Contract } from '@ethersproject/contracts'
+import { useActiveWeb3React } from "services/web3"
 
 export function usePrice(tokenAddress: String | undefined | null) {
     const priceHelperContract = usePriceHelperContract()
@@ -20,4 +22,18 @@ export function usePrice(tokenAddress: String | undefined | null) {
         return null
       }
     }, [price])
+  }
+
+
+  export function usePairPrice(pairContract?: Contract | null, pairDecimals?: number | null, invert: boolean = false) {
+    // const { account, chainId } = useActiveWeb3React()
+  
+    const result = useSingleCallResult(pairContract ? pairContract : null, 'getReserves', undefined, NEVER_RELOAD)?.result
+  
+    const _reserve1 = invert ? result?.['reserve0'] : result?.['reserve1']
+    const _reserve0 = invert ? result?.['reserve1'] : result?.['reserve0']
+  
+    const price = _reserve1 ? (Number(_reserve1) / Number(_reserve0)) * (pairDecimals ? 10 ** pairDecimals : 1) : 0
+  
+    return price
   }
