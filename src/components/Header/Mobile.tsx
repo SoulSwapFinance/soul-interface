878b117d4cc32190c4e2d31679722468da48388f
Buns <BunsDev@users.noreply.github.com>
2022-01-27 22:30:49 -0600

    remove ml

--- src/components/Header/Mobile.tsx
+++ src/components/Header/Mobile.tsx
@@ -43,7 +43,7 @@ const Mobile: FC = () => {
     <header className="flex flex-row mr-4 ml-4 justify-center w-screen flex-nowrap">
     {/* <div className="flex items-center justify-between">
           </div> */}
-        <div className="flex ml-1 p-2 justify-between">
+        <div className="flex p-2 justify-between">
             <MenuIcon width={20} className="hover:text-white text-white cursor-pointer" onClick={() => setOpen(true)} />
           <nav className={NAV_CLASS}>
           <Container maxWidth="xl" className="mx-auto">
