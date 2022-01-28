a7fc30f46b08f4261c89f5c2a29e45d162c6591c
Buns <bunsthedev@gmail.com>
2022-01-27 20:38:07 -0600

    update: navigation

--- src/components/Header/Mobile.tsx
+++ src/components/Header/Mobile.tsx
@@ -40,14 +40,14 @@ const Mobile: FC = () => {
 
   return (
     <>      
-      <header className="max-w-full flex items-center justify-between min-h-[64px] h-[64px] mr-0 ml-0">
-          <div className="p-0 hover:bg-white/10 rounded-full">
-            <MenuIcon width={20} className="hover:text-white text-white cursor-pointer" onClick={() => setOpen(true)} />
-          </div>
-        <div className="flex flex-grow justify-between">
+    <header className="flex flex-row mr-4 ml-4 justify-between w-screen flex-nowrap">
+    {/* <div className="flex items-center justify-between">
+          </div> */}
+        {/* <div className="flex p-2 justify-between"> */}
+            {/* <MenuIcon width={20} className="hover:text-white text-white cursor-pointer" onClick={() => setOpen(true)} /> */}
           <nav className={NAV_CLASS}>
-          <Container maxWidth="md" className="mx-auto">
-            <div className="flex gap-3 px-6 items-center justify-between">
+          <Container maxWidth="xl" className="mx-auto">
+            <div className="flex gap-1 px-4 items-center justify-between">
                 {mobileMenu.map((node) => {
                   return <NavigationItem node={node} key={node.key} />
                 })}
@@ -55,7 +55,7 @@ const Mobile: FC = () => {
                 </div>
           </Container>
         </nav>
-        </div>
+        {/* </div> */}
         <Transition.Root show={open} as={Fragment}>
           <Dialog as="div" className="fixed inset-0 overflow-hidden z-20" onClose={setOpen}>
             <div className="absolute inset-0 overflow-hidden">
--- src/components/Header/NavigationItem.tsx
+++ src/components/Header/NavigationItem.tsx
@@ -73,7 +73,7 @@ export const NavigationItem: FC<NavigationItem> = ({ node }) => {
               leaveFrom="transform opacity-100 scale-100"
               leaveTo="transform opacity-0 scale-95"
             >
-              <Popover.Panel className="z-10 w-full absolute w-40 translate-y-[-8px] translate-x-[-8px]">
+              <Popover.Panel className="z-10 w-full absolute w-30 translate-y-[-8px] translate-x-[-8px]">
                 <div
                   className={classNames(
                     'shadow-md shadow-black/40 border border-dark-700 rounded overflow-hidden',
@@ -90,7 +90,7 @@ export const NavigationItem: FC<NavigationItem> = ({ node }) => {
                       onClick={() => {
                         router.push(leaf.link).then(() => buttonRef?.current?.click())
                       }}
-                      className="relative px-3 py-2 hover:cursor-pointer hover:text-white m-1 rounded-lg hover:bg-white/10"
+                      className="relative px-1 py-2 hover:cursor-pointer hover:text-white m-1 rounded-lg hover:bg-white/10"
                     >
                       {leaf.title}
                     </Typography>
