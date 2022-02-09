02d8150e0d3be4e7f6533f389aec88c0b17bbec7
Buns <bunsthedev@gmail.com>
2022-02-09 05:43:54 -0600

    update [lend-sdk]: entities, constants, functions

--- src/sdk/constants/underworld.ts
+++ src/sdk/constants/underworld.ts
@@ -1,29 +1,52 @@
-import { BigNumber } from '@ethersproject/bignumber';
-export declare const ACTION_ADD_ASSET = 1;
-export declare const ACTION_REPAY = 2;
-export declare const ACTION_REMOVE_ASSET = 3;
-export declare const ACTION_REMOVE_COLLATERAL = 4;
-export declare const ACTION_BORROW = 5;
-export declare const ACTION_GET_REPAY_SHARE = 6;
-export declare const ACTION_GET_REPAY_PART = 7;
-export declare const ACTION_ACCRUE = 8;
-export declare const ACTION_ADD_COLLATERAL = 10;
-export declare const ACTION_UPDATE_EXCHANGE_RATE = 11;
-export declare const ACTION_BENTO_DEPOSIT = 20;
-export declare const ACTION_BENTO_WITHDRAW = 21;
-export declare const ACTION_BENTO_TRANSFER = 22;
-export declare const ACTION_BENTO_TRANSFER_MULTIPLE = 23;
-export declare const ACTION_BENTO_SETAPPROVAL = 24;
-export declare const ACTION_CALL = 30;
-export declare const MINIMUM_TARGET_UTILIZATION: BigNumber;
-export declare const MAXIMUM_TARGET_UTILIZATION: BigNumber;
-export declare const UTILIZATION_PRECISION: BigNumber;
-export declare const FULL_UTILIZATION: BigNumber;
-export declare const FULL_UTILIZATION_MINUS_MAX: BigNumber;
-export declare const STARTING_INTEREST_PER_YEAR: BigNumber;
-export declare const MINIMUM_INTEREST_PER_YEAR: BigNumber;
-export declare const MAXIMUM_INTEREST_PER_YEAR: BigNumber;
-export declare const INTEREST_ELASTICITY: BigNumber;
-export declare const FACTOR_PRECISION: BigNumber;
-export declare const PROTOCOL_FEE: BigNumber;
-export declare const PROTOCOL_FEE_DIVISOR: BigNumber;
+import JSBI from 'jsbi'
+
+// Functions that need accrue to be called
+export const ACTION_ADD_ASSET = 1
+export const ACTION_REPAY = 2
+export const ACTION_REMOVE_ASSET = 3
+export const ACTION_REMOVE_COLLATERAL = 4
+export const ACTION_BORROW = 5
+export const ACTION_GET_REPAY_SHARE = 6
+export const ACTION_GET_REPAY_PART = 7
+export const ACTION_ACCRUE = 8
+
+// Functions that don't need accrue to be called
+export const ACTION_ADD_COLLATERAL = 10
+export const ACTION_UPDATE_EXCHANGE_RATE = 11
+
+// Function on CoffinBox
+export const ACTION_COFFIN_DEPOSIT = 20
+export const ACTION_COFFIN_WITHDRAW = 21
+export const ACTION_COFFIN_TRANSFER = 22
+export const ACTION_COFFIN_TRANSFER_MULTIPLE = 23
+export const ACTION_COFFIN_SETAPPROVAL = 24
+
+// Any external call (except to CoffinBox)
+export const ACTION_CALL = 30
+
+export const MINIMUM_TARGET_UTILIZATION = JSBI.BigInt('700000000000000000') // 70%
+
+export const MAXIMUM_TARGET_UTILIZATION = JSBI.BigInt('800000000000000000') // 80%
+
+export const UTILIZATION_PRECISION = JSBI.BigInt('1000000000000000000')
+
+export const FULL_UTILIZATION = JSBI.BigInt('1000000000000000000')
+
+export const FULL_UTILIZATION_MINUS_MAX = JSBI.subtract(FULL_UTILIZATION, MAXIMUM_TARGET_UTILIZATION)
+
+// approx 1% APR
+export const STARTING_INTEREST_PER_YEAR = JSBI.multiply(JSBI.BigInt(317097920), JSBI.BigInt(60 * 60 * 24 * 365))
+
+// approx 0.25% APR
+export const MINIMUM_INTEREST_PER_YEAR = JSBI.multiply(JSBI.BigInt(79274480), JSBI.BigInt(60 * 60 * 24 * 365))
+
+// approx 1000% APR
+export const MAXIMUM_INTEREST_PER_YEAR = JSBI.multiply(JSBI.BigInt(317097920000), JSBI.BigInt(60 * 60 * 24 * 365))
+
+export const INTEREST_ELASTICITY = JSBI.BigInt('28800000000000000000000000000000000000000') // Half or double in 28800 seconds (8 hours) if linear
+
+export const FACTOR_PRECISION = JSBI.BigInt('1000000000000000000')
+
+export const PROTOCOL_FEE = JSBI.BigInt('10000') // 10%
+
+export const PROTOCOL_FEE_DIVISOR = JSBI.BigInt('100000')
---
+++ src/sdk/functions/underworldHooks.ts
@@ -0,0 +1,108 @@
+import { JSBI, Rebase, ZERO } from 'sdk'
+import {
+    FACTOR_PRECISION,
+    FULL_UTILIZATION_MINUS_MAX,
+    INTEREST_ELASTICITY,
+    MAXIMUM_INTEREST_PER_YEAR,
+    MAXIMUM_TARGET_UTILIZATION,
+    MINIMUM_INTEREST_PER_YEAR,
+    MINIMUM_TARGET_UTILIZATION,
+    PROTOCOL_FEE,
+    PROTOCOL_FEE_DIVISOR,
+    STARTING_INTEREST_PER_YEAR,
+  } from '../constants'
+  
+  import { BigNumber } from '@ethersproject/bignumber'
+  import { UnderworldMediumRiskLendingPair } from '../entities'
+  
+export function toAmount(token: Rebase, shares: JSBI): JSBI {
+    return JSBI.GT(token.base, 0) ? JSBI.divide(JSBI.multiply(shares, token.elastic), token.base) : ZERO
+  }
+  
+  export function toShare(token: Rebase, amount: JSBI): JSBI {
+    return JSBI.GT(token.elastic, 0) ? JSBI.divide(JSBI.multiply(amount, token.base), token.elastic) : ZERO
+  }
+
+
+export function accrue(pair: UnderworldMediumRiskLendingPair, amount: JSBI, includePrincipal = false): JSBI {
+    return JSBI.add(
+      JSBI.divide(
+        JSBI.multiply(JSBI.multiply(amount, pair.accrueInfo.interestPerSecond), pair.elapsedSeconds),
+        JSBI.BigInt(1e18)
+      ),
+      includePrincipal ? amount : ZERO
+    )
+  }
+  
+  export function accrueTotalAssetWithFee(pair: UnderworldMediumRiskLendingPair): {
+    elastic: JSBI
+    base: JSBI
+  } {
+    const extraAmount = JSBI.divide(
+      JSBI.multiply(
+        JSBI.multiply(pair.totalBorrow.elastic, pair.accrueInfo.interestPerSecond),
+        JSBI.add(pair.elapsedSeconds, JSBI.BigInt(3600)) // For some transactions, to succeed in the next hour (and not only this block), some margin has to be added
+      ),
+      JSBI.BigInt(1e18)
+    )
+    const feeAmount = JSBI.divide(JSBI.multiply(extraAmount, PROTOCOL_FEE), PROTOCOL_FEE_DIVISOR) // % of interest paid goes to fee
+    const feeFraction = JSBI.divide(JSBI.multiply(feeAmount, pair.totalAsset.base), pair.currentAllAssets)
+    return {
+      elastic: pair.totalAsset.elastic,
+      base: JSBI.add(pair.totalAsset.base, feeFraction),
+    }
+  }
+  
+  export function interestAccrue(pair: UnderworldMediumRiskLendingPair, interest: JSBI): JSBI {
+    if (JSBI.equal(pair.totalBorrow.base, ZERO)) {
+      return STARTING_INTEREST_PER_YEAR
+    }
+    if (JSBI.lessThanOrEqual(pair.elapsedSeconds, ZERO)) {
+      return interest
+    }
+  
+    let currentInterest = interest
+  
+    if (JSBI.lessThan(pair.utilization, MINIMUM_TARGET_UTILIZATION)) {
+      const underFactor = JSBI.greaterThan(MINIMUM_TARGET_UTILIZATION, ZERO)
+        ? JSBI.divide(
+            JSBI.multiply(JSBI.subtract(MINIMUM_TARGET_UTILIZATION, pair.utilization), FACTOR_PRECISION),
+            MINIMUM_TARGET_UTILIZATION
+          )
+        : ZERO
+      const scale = JSBI.add(
+        INTEREST_ELASTICITY,
+        JSBI.multiply(JSBI.multiply(underFactor, underFactor), pair.elapsedSeconds)
+      )
+      currentInterest = JSBI.divide(JSBI.multiply(currentInterest, INTEREST_ELASTICITY), scale)
+  
+      if (JSBI.lessThan(currentInterest, MINIMUM_INTEREST_PER_YEAR)) {
+        currentInterest = MINIMUM_INTEREST_PER_YEAR // 0.25% APR minimum
+      }
+    } else if (JSBI.greaterThan(pair.utilization, MAXIMUM_TARGET_UTILIZATION)) {
+      const overFactor = JSBI.multiply(
+        JSBI.subtract(pair.utilization, MAXIMUM_TARGET_UTILIZATION),
+        JSBI.divide(FACTOR_PRECISION, FULL_UTILIZATION_MINUS_MAX)
+      )
+      const scale = JSBI.add(
+        INTEREST_ELASTICITY,
+        JSBI.multiply(JSBI.multiply(overFactor, overFactor), pair.elapsedSeconds)
+      )
+      currentInterest = JSBI.divide(JSBI.multiply(currentInterest, scale), INTEREST_ELASTICITY)
+      if (JSBI.greaterThan(currentInterest, MAXIMUM_INTEREST_PER_YEAR)) {
+        currentInterest = MAXIMUM_INTEREST_PER_YEAR // 1000% APR maximum
+      }
+    }
+    return currentInterest
+  }
+  
+  // Subtract protocol fee
+  export function takeFee(amount: JSBI): JSBI {
+    return JSBI.subtract(amount, JSBI.divide(JSBI.multiply(amount, PROTOCOL_FEE), PROTOCOL_FEE_DIVISOR))
+  }
+  
+  export function addBorrowFee(amount: BigNumber): BigNumber {
+    return amount.mul(BigNumber.from(10005)).div(BigNumber.from(10000))
+  }
+  
+  export { computePairAddress } from './computePairAddress'
