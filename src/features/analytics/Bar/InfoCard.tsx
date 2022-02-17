c85a7e9941a253357e31c9f68923c382406dda5b
Buns <bunsthedev@gmail.com>
2022-02-17 05:58:12 -0600

    update: CoffinBox Analytics

--- src/features/analytics/Bar/InfoCard.tsx
+++ src/features/analytics/Bar/InfoCard.tsx
@@ -7,9 +7,9 @@ interface InfoCardProps {
 
 export default function InfoCard({ text, number }: InfoCardProps) {
   return (
-    <div className="w-full py-3 border rounded px-9 bg-dark-900 border-dark-700">
-      <div className="whitespace-nowrap">{text}</div>
-      <div className="text-2xl font-bold">{number}</div>
+    <div className="w-full py-3 border rounded px-9 bg-dark-1000 border-dark-700">
+      <div className="text-center whitespace-nowrap">{text}</div>
+      <div className="text-center text-2xl font-bold">{number}</div>
     </div>
   )
 }
