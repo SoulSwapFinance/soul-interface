f6c59ad3f8b76de2456b4816989c0550fea3c52a
Buns <bunsthedev@gmail.com>
2022-03-06 23:18:06 -0600

    fix: address

--- src/constants/farms.ts
+++ src/constants/farms.ts
@@ -874,7 +874,7 @@ export const POOLS: AddressMap = {
     },
     '0xbDa9204e6D596feCf9bd48108723F9BDAa2019f6': {  // BNB/FTM
       id: 53, // 420
-      address: '0x74b23882a30290451a17c44f4f05243b6b58c76d',
+      address: '0xd67de0e0a0fd7b15dc8348bb9be742f3c5850454',
       type: 'underworld',
       token0: {
         id: '0xD67de0e0a0Fd7b15dC8348Bb9BE742F3c5850454', // BNB
