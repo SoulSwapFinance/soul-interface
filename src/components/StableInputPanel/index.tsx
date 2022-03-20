e9a326a4ca9dfdaf16735f28af8c4512e71c99cc
Buns <bunsthedev@gmail.com>
2022-03-20 00:37:50 -0500

    update: StableAssetInput precision (2)

--- src/components/StableInputPanel/index.tsx
+++ src/components/StableInputPanel/index.tsx
@@ -163,7 +163,7 @@ export default function StableInputPanel({
                         {/* {i18n._(t`Balance:`)}  */}
                         {/* {i18n._(t`Balance:`)}  */}
                         {/* {formatCurrencyAmount(selectedCurrencyBalance, 4)} {currency.symbol} */}
-                        {selectedCurrencyBalance?.toSignificant(6, { groupSeparator: ',' }) || '0'} {currency?.symbol}
+                        {selectedCurrencyBalance?.toSignificant(2, { groupSeparator: ',' }) || '0'} {currency?.symbol}
                       </>
                     )}
                   </div>
