7df0d8bf2ead7953b1045a9fdb82c28a1176aef4
Buns <BunsDev@users.noreply.github.com>
2022-03-27 00:13:04 -0500

    try: AssetInput

--- src/pages/luxor/stake/index.tsx
+++ src/pages/luxor/stake/index.tsx
@@ -404,16 +404,24 @@ export default function Stablecoin() {
                 />
               </Button> */}
 
-              <StableInputPanel
+             {/* <StableInputPanel
                 value={redeemValue}
                 showMaxButton={true}
                 onUserInput={(value) => setRedeemValue(value)}
                 onMax={ () => setRedeemValue(lumensBalance.toExact()) }
-                // currency={lumensToken}
+                currency={lumensToken}
                 disableCurrencySelect={true}
                 locked={!account}
                 id="stablecoin-currency-input"
-              />
+              /> */}
+               <AssetInput
+                            currencyLogo={false}
+                                currency={lumensToken}
+                                currencyAddress={lumensToken.address}
+                                value={value}
+                                onChange={setRedeemValue}
+                                showMax={false}
+                            />
               <div className="h-px my-6 bg-dark-1000"></div>
               <div className="flex flex-col bg-dark-1000 p-3 border border-1 border-dark-700 hover:border-yellow w-full space-y-1">
               <div className="flex justify-between">
