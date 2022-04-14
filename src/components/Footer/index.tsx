04d1c44588e797c776180cd213328e1e576cca07
Buns <bunsthedev@gmail.com>
2022-04-14 07:17:53 -0500

    use modals

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -10,8 +10,8 @@ const Footer = () => {
   const { chainId } = useActiveWeb3React()
   const { i18n } = useLingui()
   return (
-    // <footer className="absolute bottom-0 flex items-center justify-between w-screen h-20 p-4 mx-auto text-center text-low-emphesis">
-    <footer className="flex-shrink-0 w-full">
+    <footer className="absolute bottom-0 flex items-center justify-between w-screen h-20 p-4 mx-auto text-center text-low-emphesis">
+    {/* <footer className="flex-shrink-0 w-full"> */}
       <div className="flex bottom-0 items-center justify-between h-20 px-4 bg-dark-1200">
         {chainId && chainId in ANALYTICS_URL && (
           <ExternalLink
