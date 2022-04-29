c05199f8cd730b99bc62a356d100f68269c941f2
Buns <bunsthedev@gmail.com>
2022-04-29 02:06:40 -0500

    update style: Lending Markets Page

--- src/features/underworld/List.tsx
+++ src/features/underworld/List.tsx
@@ -70,7 +70,7 @@ export const List = () => {
           </NavLink>
         </Button>
       </div>
-      <Typography className="text-2xl mb-12 mt-6 border border-blue p-3 hover:border-dark-600 font-bold text-center">
+     <Typography className="text-2xl rounded bg-dark-1000 mb-3 mt-6 border border-blue p-3 font-bold text-center">
         Lending Markets
       </Typography>
         <Underworld />
