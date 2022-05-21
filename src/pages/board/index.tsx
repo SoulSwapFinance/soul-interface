5d79ed1b8935bb9683f59bbca3b68b6696c44fa5
Buns <bunsthedev@gmail.com>
2022-05-21 17:50:46 -0500

    add: Board Page

---
+++ src/pages/board/index.tsx
@@ -0,0 +1,15 @@
+// import Container from "components/Container";
+import React from "react";
+
+const Board = () => {
+	return (
+      <iframe 
+			frameBorder={"none"}
+    		title={"Board"}
+    		src="https://soulswap.nolt.io/widget"
+    		height={"900px"}
+    		width={"100%"}
+    />
+	);
+};
+export default Board;
