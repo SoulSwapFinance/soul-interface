4f7574bf63e59c7581a935708c7cf6ce7d5767cf
Buns <bunsthedev@gmail.com>
2022-06-03 02:28:01 -0500

    add Page: Crosschain (wip)

---
+++ src/pages/crosschain/components/InputCurrencyBox.tsx
@@ -0,0 +1,75 @@
+import React, { useContext, useState } from "react";
+import InputCurrency from "./InputCurrency";
+import { Button } from "components/index";
+import Row from "components/Row";
+
+export interface Token {
+  name: string;
+  symbol: string;
+  address: string;
+  decimals: number;
+  logoURL: string;
+  totalSupply?: string;
+  balanceOf?: string;
+}
+
+export const FANTOM_NATIVE: Token = {
+  address: null,
+  decimals: 18,
+  name: "Fantom",
+  symbol: "FTM",
+  logoURL: "https://raw.githubusercontent.com/SoulSwapFinance/assets/master/blockchains/fantom/Fantom.svg",
+};
+
+const InputCurrencyBox: React.FC<any> = ({
+  value,
+  max,
+  setValue,
+  disabled,
+  minus,
+  variant = "old",
+}) => {
+  const [error, setError] = useState(null);
+  const handleSetMax = () => {
+    setError(null);
+    setValue(max);
+  };
+
+  return (
+    <Row
+      style={{
+        width: "100%",
+        backgroundColor: variant === "new" ? "black" : "#202F49",
+        borderRadius: "8px",
+        height: "64px",
+        alignItems: "center",
+      }}
+    >
+      <div />
+      <InputCurrency
+        disabled={disabled}
+        value={value}
+        max={max}
+        handleValue={setValue}
+        handleError={setError}
+        token={ FANTOM_NATIVE }
+      />
+      <Row style={{ alignItems: "center" }}>
+        <Button
+          disabled={ disabled }
+          fontSize="14px"
+          color={ "black" }
+          padding="6px"
+          style={{ flex: 1 }}
+          variant="tertiary"
+          onClick={handleSetMax}
+        >
+          MAX {minus ? "" : ""}
+        </Button>
+        <div className="mr-2" />
+      </Row>
+    </Row>
+  );
+};
+
+export default InputCurrencyBox;
---
+++ src/pages/crosschain/components/Modal.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+import ModalClose from "./ModalClose";
+
+export interface ModalProps {
+  onDismiss?: () => void;
+}
+
+const Modal: React.FC<any> = ({ children, style, onDismiss }) => {
+  return (
+    <StyledResponsiveWrapper>
+      <StyledModal style={{ ...style }}>
+        {onDismiss && <ModalClose onDismiss={onDismiss} />}
+        {children}
+      </StyledModal>
+    </StyledResponsiveWrapper>
+  );
+};
+
+const mobileKeyframes = keyframes`
+  0% {
+    transform: translateY(0%);
+  }
+  100% {
+    transform: translateY(-100%);
+  }
+`;
+
+const StyledResponsiveWrapper = styled.div<any>`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  position: relative;
+
+  @media (max-width: 400px) {
+    flex: 1;
+    top: 100%;
+    right: 0;
+    left: 0;
+    max-height: 100%;
+    animation: ${mobileKeyframes} 0.3s forwards ease-out;
+    max-width: 100vw;
+  }
+`;
+
+const StyledModal = styled.div<any>`
+  padding: ${(props) =>
+    props.padding === undefined ? "20px 40px" : props.padding};
+  background: #821FFF;
+  color: white;
+  font-family: "proxima-nova", sans-serif;
+  border-radius: 8px;
+  backdrop-filter: blur(40px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  min-height: 0;
+  min-width: 20rem;
+  z-index: 1;
+`;
+
+export default Modal;
