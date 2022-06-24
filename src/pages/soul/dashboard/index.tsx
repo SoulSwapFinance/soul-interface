bbe1d5b4ce1c2541db691d36637db3d088be3f79
Buns <BunsDev@users.noreply.github.com>
2022-06-23 21:38:50 -0500

    hide name on mobile Dashboard

--- src/pages/soul/dashboard/index.tsx
+++ src/pages/soul/dashboard/index.tsx
@@ -186,6 +186,12 @@ export default function Dashboard() {
   // const toggleSorModal = useToggleModal(ApplicationModal.SOR)
   // const toggleWrapModal = useToggleModal(ApplicationModal.WRAP)
 
+ const HideOnMobile = styled(ItemBox)`
+  @media screen and (max-width: 900px) {
+    display: none;
+   }
+ `;
+
   return (
     <Container id="dashboard-page" className="py-4 space-y-4 md:py-8 max-w-min">
       <DoubleGlowShadowV2>
@@ -194,15 +200,15 @@ export default function Dashboard() {
         <meta key="description" name="description" />
       </Head>
       <div className="grid grid-cols justify-center">
-
-     {/* <div className="py-1 mb-1 mt-3 ml-3 mr-3 bg-dark-600" />
+<HideOnMobile>
+      <div className="py-1 mb-1 mt-3 ml-3 mr-3 bg-dark-600" />
         <div className="py-1 mb-1 ml-3 mr-3  bg-purple" />
         <Typography variant="h1" className="text-center mt-4 mb-4 text-dark-600" component="h1">
           SOULSWAP FINANCE
         </Typography>
         <div className="py-1 mt-1 ml-3 mr-3 bg-dark-600" />
         <div className="py-1 mt-1 ml-3 mr-3 mb-3 bg-purple" />
-        */}
+        </HideOnMobile>
 
       <div className="flex ml-4 mr-4 mb-4 gap-1 items-center justify-center">
         <Button variant="bordered" color="purple" size="lg">
