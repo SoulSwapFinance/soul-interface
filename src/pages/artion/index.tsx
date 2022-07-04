55aade6f73be1e09545b50c225db9a837f0123f0
Buns <bunsthedev@gmail.com>
2022-07-03 21:44:17 -0500

    test-iFrames

--- src/pages/artion/index.tsx
+++ src/pages/artion/index.tsx
@@ -6,8 +6,8 @@ const Nfnt = () => {
       <iframe 
 			frameBorder={"none"}
     		title={"NFNT"}
-    		// src="https://nfnt.art/"
-    		src="https://reservoir-nft-marketplace-interface.vercel.app/0x08d7c0242953446436f34b4c78fe9da38c73668d/300"
+    		src="https://artion-client-v2.vercel.app/"
+    		// src="https://reservoir-nft-marketplace-interface.vercel.app/0x08d7c0242953446436f34b4c78fe9da38c73668d/300"
     		height={"900px"}
     		width={"100%"}
     />
