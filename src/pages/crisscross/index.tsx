1f3688bdd93fb957fc82c90db29ddeff0f74c01d
Buns <bunsthedev@gmail.com>
2022-08-13 22:42:29 -0500

    test: lifi-web-embedded

---
+++ src/pages/crisscross/index.tsx
@@ -0,0 +1,16 @@
+const CrissCross = () => {
+	return (
+        <iframe
+        id="crisscross-widget-iframe"
+        title="Swap Widget"
+        height="900px"
+        width="100%"
+        // style="border: none; border-radius: 19px; box-shadow: 3px 3px 10px 4px rgba(0, 0, 0, 0.1); display: none;"
+        src="https://lifi-web.vercel.app"
+        // onload="onFrameLoad()"
+    >
+    </iframe>
+	);
+};
+
+export default CrissCross;
