bc0353f67ed905618217ce566e04859964b1fed9
Buns <bunsthedev@gmail.com>
2022-08-16 16:11:10 -0500

    add: crissembedded

--- src/pages/crissembedded/index.tsx
+++ src/pages/crissembedded/index.tsx
@@ -6,7 +6,7 @@ const CrissEmbedded = () => {
         height="900px"
         width="100%"
         // style="border: none; border-radius: 19px; box-shadow: 3px 3px 10px 4px rgba(0, 0, 0, 0.1); display: none;"
-        src="https://lifi-web.vercel.app/embedded"
+        src="https://lifi-web.vercel.app/swap-v2"
         // onload="onFrameLoad()"
     >
     </iframe>
