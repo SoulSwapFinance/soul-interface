ef26624f8f975cdc4dc2ac788b6dfc4a44af68b5
Buns <bunsthedev@gmail.com>
2022-08-22 16:54:32 -0500

    add: Radio Component, SwapNavTab, ListFilter

---
+++ src/components/Pool/ListFilter.tsx
@@ -0,0 +1,68 @@
+import React, { ChangeEvent, useCallback } from 'react'
+import { useWeb3React } from '@web3-react/core'
+import { Box, Flex } from 'rebass'
+import styled from 'styled-components'
+
+import Radio from 'components/Radio'
+
+export enum PairsFilterType {
+  ALL = 'ALL',
+  REWARDS = 'REWARDS',
+  MY = 'MY',
+  SOULSWAP = 'SOULSWAP',
+}
+
+const StyledRoot = styled(Flex)<{ disabled?: boolean }>`
+  opacity: ${props => (props.disabled ? 0.2 : 1)};
+  pointer-events: ${({ disabled }) => (disabled ? 'none' : 'auto')};
+`
+
+interface ListFilterProps {
+  filter: PairsFilterType
+  disabled?: boolean
+  onFilterChange: (newFilter: PairsFilterType) => void
+}
+
+export default function ListFilter({ disabled, filter, onFilterChange }: ListFilterProps) {
+  const { account } = useWeb3React()
+
+  const handleFilterRadioChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      onFilterChange(PairsFilterType[event.target.value as keyof typeof PairsFilterType])
+    },
+    [onFilterChange]
+  )
+
+  return (
+    <StyledRoot justifyContent="space-between" disabled={disabled}>
+      <Flex flex="1" flexWrap="wrap">
+        <Box mb={['8px', '0px']} mr="24px">
+          <Radio
+            onChange={handleFilterRadioChange}
+            checked={filter === PairsFilterType.ALL}
+            label="All pairs"
+            value={PairsFilterType.ALL.toString()}
+          />
+        </Box>
+        {!!account && (
+          <Box mr="24px">
+            <Radio
+              onChange={handleFilterRadioChange}
+              checked={filter === PairsFilterType.MY}
+              label="My pairs"
+              value={PairsFilterType.MY.toString()}
+            />
+          </Box>
+        )}
+        <Box mr="24px">
+          <Radio
+            onChange={handleFilterRadioChange}
+            checked={filter === PairsFilterType.REWARDS}
+            label="With rewards"
+            value={PairsFilterType.REWARDS.toString()}
+          />
+        </Box>
+      </Flex>
+    </StyledRoot>
+  )
+}
---
+++ src/hooks/useNativeCurrency.ts
@@ -0,0 +1,11 @@
+import { ChainId, WETH9, Currency, NATIVE } from 'sdk'
+
+import { useActiveWeb3React } from './index'
+
+export function useNativeCurrency(chainId?: ChainId): Currency {
+  const { chainId: activeChainId } = useActiveWeb3React()
+  const selectedChainId = chainId ?? activeChainId
+  // fallback to ether if chain id is not defined
+  if (!selectedChainId) return WETH9[chainId || 250]
+  return NATIVE[selectedChainId || 250] ||WETH9[selectedChainId || 250]
+}
