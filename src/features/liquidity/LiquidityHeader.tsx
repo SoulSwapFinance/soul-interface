8b404a1350a0daa8e01cfa9193bd9e868eb9a949
Buns <bunsthedev@gmail.com>
2022-09-10 18:25:32 -0500

    liquidity: maintain header color

--- src/features/liquidity/LiquidityHeader.tsx
+++ src/features/liquidity/LiquidityHeader.tsx
@@ -11,7 +11,7 @@ export default function LiquidityHeader({ input = undefined, output = undefined 
     <div className="grid grid-cols-2 rounded-md p-3px bg-dark-800">
       <NavLink
         activeClassName={classNames("font-bold text-high-emphesis", `bg-${getChainColorCode(chainId)}`)}
-        href={`/add/${currencyId(input)}/${currencyId(output)}`}
+        href={`/exchange/add/${currencyId(input)}/${currencyId(output)}`}
       >
         <a className="flex items-center justify-center px-1 py-1 text-base font-medium text-center rounded-md md:px-10 text-secondary hover:text-high-emphesis">
           Add
@@ -22,7 +22,7 @@ export default function LiquidityHeader({ input = undefined, output = undefined 
           if (!output) event.preventDefault()
         }}
         activeClassName={classNames("text-high-emphesis font-bold", `bg-${getChainColorCode(chainId)}`)}
-        href={`/remove/${currencyId(input)}/${currencyId(output)}`}
+        href={`/exchange/remove/${currencyId(input)}/${currencyId(output)}`}
       >
         <a className="flex items-center justify-center px-1 py-1 text-base font-medium text-center rounded-md md:px-10 text-secondary hover:text-high-emphesis">
           Remove
