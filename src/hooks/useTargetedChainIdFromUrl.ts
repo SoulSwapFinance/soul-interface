48f52067c6be00f10bcb33252e3e93050bd09cd9
Buns <bunsthedev@gmail.com>
2022-09-10 17:10:43 -0500

    add hook: useTargetedChainIdFromUrl

---
+++ src/hooks/useTargetedChainIdFromUrl.ts
@@ -0,0 +1,12 @@
+import { ChainId } from 'sdk'
+
+import useParsedQueryString from './useParsedQueryString'
+
+export function useTargetedChainIdFromUrl(): ChainId | undefined {
+  const { chainId: targetedChainId } = useParsedQueryString()
+  if (typeof targetedChainId === 'string') {
+    const parsedTargetedChainId = parseInt(targetedChainId)
+    return Object.values(ChainId).indexOf(parsedTargetedChainId) >= 0 ? parsedTargetedChainId : undefined
+  }
+  return undefined
+}
