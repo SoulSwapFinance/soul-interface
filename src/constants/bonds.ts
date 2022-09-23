d45b2d22249052c4bc2ab6a203908432a1a4d909
Buns <68980965+BunsDev@users.noreply.github.com>
2022-09-22 20:28:57 -0500

    add: Avalanche bonds (wip)

--- src/constants/bonds.ts
+++ src/constants/bonds.ts
@@ -1,5 +1,5 @@
 import { BNB_ADDRESS, WBTC_ADDRESS, WETH_ADDRESS } from 'constants/addresses'
-import { ChainId, DAI_ADDRESS, SEANCE_ADDRESS, SOUL_ADDRESS, USDC_ADDRESS, WNATIVE_ADDRESS } from '../sdk'
+import { ChainId, DAI_ADDRESS, SEANCE_ADDRESS, SOUL_ADDRESS, SOUL_NATIVE_ADDRESS, USDC_NATIVE_ADDRESS, USDC_ADDRESS, WNATIVE_ADDRESS } from '../sdk'
 
 export type TokenInfo = {
   id: string
@@ -23,8 +23,44 @@ type AddressMap = {
 }
 
 export const BONDS: AddressMap = {
+
+// AVALANCHE BONDS //
+  [ChainId.AVALANCHE]: {
+    SOUL_NATIVE_ADDRESS[ChainId.AVALANCHE]: { // SOUL-AVAX
+      id: 0, // 250
+      token0: {
+        id: WNATIVE_ADDRESS[ChainId.AVALANCHE],
+        name: 'Wrapped Avalanche',
+        symbol: 'WFTM',
+        decimals: 18,
+      },
+      token1: {
+        id: SOUL_ADDRESS[ChainId.AVALANCHE],
+        name: 'Soul Power',
+        symbol: 'SOUL',
+        decimals: 18,
+      },
+    },
+    '0x922fcADa825Dc669798206A35D2D2B455f9A64E7': { // SOUL-USDC
+      id: 1, // 150
+      token0: {
+        id: SOUL_ADDRESS[ChainId.AVALANCHE],
+        name: 'Soul Power',
+        symbol: 'SOUL',
+        decimals: 18,
+      },
+      token1: {
+        id: USDC_ADDRESS[ChainId.AVALANCHE],
+        name: 'USD Coin',
+        symbol: 'USDC',
+        decimals: 6,
+      },
+    },
+  },
+ 
+// FANTOM BONDS //
   [ChainId.FANTOM]: {
-    '0xa2527Af9DABf3E3B4979d7E0493b5e2C6e63dC57': { // SOUL-FTM
+SOUL_NATIVE_ADDRESS[ChainId.FANTOM]: { // SOUL-FTM
       id: 0, // 2400
       token0: {
         id: WNATIVE_ADDRESS[ChainId.FANTOM],
@@ -69,7 +105,7 @@ export const BONDS: AddressMap = {
         decimals: 18,
       },
     },
-    '0x160653F02b6597E7Db00BA8cA826cf43D2f39556': { // FTM-USDC 
+    USDC_NATIVE_ADDRESS[ChainId.FANTOM]: { // FTM-USDC 
       id: 3,
       token0: {
         id: USDC_ADDRESS[ChainId.FANTOM],
@@ -159,6 +195,5 @@ export const BONDS: AddressMap = {
         decimals: 6,
       },
     },
-
   }
 }
