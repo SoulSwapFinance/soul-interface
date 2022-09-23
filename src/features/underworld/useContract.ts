a700a69d0e24ea503456bc1029e86e4571139599
Buns <68980965+BunsDev@users.noreply.github.com>
2022-09-23 02:38:35 -0500

    fix

--- src/features/underworld/useContract.ts
+++ src/features/underworld/useContract.ts
@@ -87,7 +87,7 @@ export function useSoulContract(withSignerIfPossible = true): Contract | null {
 
 export function useSoulBondContract(withSignerIfPossible?: boolean): Contract | null {
   const { chainId } = useActiveWeb3React()
-  return useContract(chainId && SOUL_BOND_ADDRESS, SOUL_BOND_ABI, withSignerIfPossible)
+  return useContract(chainId && SOUL_BOND_ADDRESS[chainId], SOUL_BOND_ABI, withSignerIfPossible)
 }
 
 export function useSoulSummonerContract(withSignerIfPossible?: boolean): Contract | null {
