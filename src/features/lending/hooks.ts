9e6c142a608f63f83160d302d9ddb494f630597b
Buns <bunsthedev@gmail.com>
2022-10-11 16:44:25 -0500

    update: Lending Layout

--- src/features/lending/hooks.ts
+++ src/features/lending/hooks.ts
@@ -136,8 +136,7 @@ export function useUnderworldPairsForAccount(account: string | null | undefined,
 
   // TODO: Replace
   // @ts-ignore TYPE NEEDS FIXING
-  const pollUnderworldPairs 
-    = useSingleCallResult(boringHelperContract, 'pollKashiPairs', pollArgs, { blocksPerFetch: 0 })?.result?.[0]
+  const pollUnderworldPairs = useSingleCallResult(boringHelperContract, 'pollKashiPairs', pollArgs, { blocksPerFetch: 0 })?.result?.[0]
 
   const strategies = useCoffinStrategies({ chainId })
 
