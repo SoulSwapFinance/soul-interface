18fa5a0df495650136a7899b30724613c8692e11
Buns <68980965+BunsDev@users.noreply.github.com>
2022-10-19 02:31:33 -0500

    add: TokenInfo page

---
+++ src/pages/tokenInfo/index.tsx
@@ -0,0 +1,15 @@
+// import Container from "components/Container";
+import React from "react";
+
+const TokenInfo = () => {
+	return (
+      <iframe 
+			frameBorder={"none"}
+    		title={"TokenInfo"}
+    		src="https://uniswap-interface-hn70qb8g7-bunsdev.vercel.app/#/tokens/ethereum"
+    		height={"900px"}
+    		width={"100%"}
+    />
+	);
+};
+export default TokenInfo
