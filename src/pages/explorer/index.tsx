d7461989dea2aa36563b94e465434fa612e427cb
Buns <bunsthedev@gmail.com>
2022-10-19 22:19:11 -0500

    add: explorer

---
+++ src/pages/explorer/index.tsx
@@ -0,0 +1,44 @@
+// import Container from "components/Container";
+import React from "react";
+// import { styled } from 'styled-components'
+
+const Explorer = () => {
+	return (
+		<div className="w-full justify-center">
+		<div className="md:hidden sm:flex sm:w-full sm:h-full">
+			<iframe
+				frameBorder={"none"}
+				title={"Explorer"}
+				src="https://explore.soulswap.finance"
+				// src="https://soulswap.finance"
+				height={"900px"}
+				width={'100%'}
+				/>
+		</div>
+	
+		<div className="xl:hidden lg:flex lg:w-full lg:h-full">
+			<iframe
+				frameBorder={"none"}
+				title={"Explorer"}
+				src="https://explore.soulswap.finance"
+				// src="https://soulswap.finance"
+				height={"1200px"}
+				width={'100%'}
+				/>
+		</div>
+
+		<div className="lg:flex lg:w-full lg:h-full">
+			<iframe
+				frameBorder={"none"}
+				title={"Explorer"}
+				src="https://explore.soulswap.finance"
+				// src="https://soulswap.finance"
+				height={"3600px"}
+				width={'100%'}
+				/>
+		</div>
+		</div>
+
+	);
+};
+export default Explorer;
