2d87985f06ab8ebd08b610a1bd31ca48c484fec2
Buns <68980965+BunsDev@users.noreply.github.com>
2022-10-30 02:33:17 -0500

    new page (wip): ecoswap

---
+++ src/pages/ecoswap/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const EcoSwap = () => {
+	return (
+      <iframe 
+			frameBorder={"none"}
+    		title={"EcoSwap"}
+    		src="https://ecoswap-ui.vercel.app"
+    		height={"900px"}
+    		width={"100%"}
+    />
+	);
+};
+
+export default EcoSwap;
