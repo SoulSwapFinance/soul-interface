3353bb7140e2b3800e1aff7cb629663c7ca08efa
Buns <bunsthedev@gmail.com>
2022-10-31 15:56:18 -0500

    format: pending rewards

--- src/features/summoner/List.tsx
+++ src/features/summoner/List.tsx
@@ -63,7 +63,7 @@ export const FarmList = () => {
 
   const avaxLendList = AvalancheLendingPools.map((farm) => (
     <ActiveRow
-      key={farm.pid} 
+      key={farm.pid}
       pid={farm.pid}
       decimals={farm.decimals}
       pairType={farm.type}
@@ -122,60 +122,69 @@ export const FarmList = () => {
     <div>
       <div className="flex ml-2 mt-2 mr-2 mb-4 gap-1 items-center justify-center">
         <NavLink href={'/soul/dashboard'}>
-        <Button variant="filled" 
-        color={"purple"} 
-        size="lg">
+          <Button variant="filled"
+            color={"purple"}
+            size="lg">
             <a className="block text-md md:text-xl text-white text-bold p-0 -m-3 text-md transition duration-150 ease-in-out rounded-md hover:bg-dark-300">
               <span> Data </span>
             </a>
-        </Button>
-          </NavLink>
+          </Button>
+        </NavLink>
         <NavLink href={'/bonds'}>
-        <Button variant="filled" 
-          color={"purple"} 
-        size="lg">
+          <Button variant="filled"
+            color={"purple"}
+            size="lg">
             <a className="block text-md md:text-xl text-white text-bold p-0 -m-3 text-md transition duration-150 ease-in-out rounded-md hover:bg-dark-300">
               <span> Bond </span>
             </a>
-        </Button>
-          </NavLink>
+          </Button>
+        </NavLink>
         <NavLink href={'/lend'}>
-        <Button variant="filled"
-          color={"purple"}  
-        size="lg"
-        >
+          <Button variant="filled"
+            color={"purple"}
+            size="lg"
+          >
             <a className="block text-md md:text-xl text-white text-bold p-0 -m-3 text-md transition duration-150 ease-in-out rounded-md hover:bg-dark-300">
               <span> Lend </span>
             </a>
-        </Button>
-          </NavLink>
-          <NavLink href={'/autostake'}>
-        <Button variant="filled" 
-        color={"purple"} 
-        // color={getChainColorCode(chainId)} 
-        size="lg"
-        >
+          </Button>
+        </NavLink>
+        <NavLink href={'/borrow'}>
+          <Button variant="filled"
+            color={"purple"}
+            size="lg"
+          >
+            <a className="block text-md md:text-xl text-white text-bold p-0 -m-3 text-md transition duration-150 ease-in-out rounded-md hover:bg-dark-300">
+              <span> Borrow </span>
+            </a>
+          </Button>
+        </NavLink>
+        <NavLink href={'/autostake'}>
+          <Button variant="filled"
+            color={"purple"}
+            size="lg"
+          >
             <a className="block text-md md:text-xl text-white text-bold p-0 -m-3 text-md transition duration-150 ease-in-out rounded-md hover:bg-dark-300">
               <span> Vault </span>
             </a>
-        </Button>
-          </NavLink>
+          </Button>
+        </NavLink>
       </div>
       <Typography className="text-2xl bg-dark-1000 mt-6 border border-dark-600 p-3 font-bold text-center">SoulSwap Pools</Typography>
       <Active />
       <>{chainId == ChainId.FANTOM ? ftmList : avaxList}</>
-        <div>
-          <Typography className="text-2xl bg-dark-1000 mt-6 border border-blue p-3 font-bold text-center">Lending Pools</Typography>
-      <Underworld />
-          {chainId == ChainId.FANTOM ? ftmLendList : avaxLendList}
-          <Typography 
-            className={classNames(chainId == ChainId.AVALANCHE ? 'hidden' : `text-2xl bg-dark-1000 mt-6 border border-pink p-3 font-bold text-center`)}
-          >
-              Retired Pools
-            </Typography>
-            {chainId == ChainId.FANTOM ?  <Inactive/> : null }
-            {chainId == ChainId.FANTOM ? inactiveFtmList : inactiveAvaxList}
-        </div>
+      <div>
+        <Typography className="text-2xl bg-dark-1000 mt-6 border border-blue p-3 font-bold text-center">Lending Pools</Typography>
+        <Underworld />
+        {chainId == ChainId.FANTOM ? ftmLendList : avaxLendList}
+        <Typography
+          className={classNames(chainId == ChainId.AVALANCHE ? 'hidden' : `text-2xl bg-dark-1000 mt-6 border border-pink p-3 font-bold text-center`)}
+        >
+          Retired Pools
+        </Typography>
+        {chainId == ChainId.FANTOM ? <Inactive /> : null}
+        {chainId == ChainId.FANTOM ? inactiveFtmList : inactiveAvaxList}
+      </div>
     </div>
   )
 }
