6010e6f583575f06a2e8b28a35a4cd395917bfa3
Buns <bunsthedev@gmail.com>
2022-11-28 21:50:07 -0600

    add: Add Liqiudity Button (Analytics)

--- src/pages/analytics/pairs/[id].tsx
+++ src/pages/analytics/pairs/[id].tsx
@@ -24,7 +24,7 @@ import { getChainColorCode } from 'constants/chains'
 import Typography from 'components/Typography'
 import { TridentHeader } from 'layouts/Trident'
 import { computePairAddress } from 'sdk/functions/computePairAddress'
-import { Currency, FACTORY_ADDRESS, Token } from 'sdk'
+import { Currency, FACTORY_ADDRESS, NATIVE, Token, WNATIVE_ADDRESS } from 'sdk'
 
 const chartTimespans = [
   {
@@ -56,11 +56,11 @@ export default function Pair({ inputCurrency, outputCurrency }: PairProps) {
   const router = useRouter()
   const id = (inputCurrency && outputCurrency)
     ? inputCurrency && outputCurrency && computePairAddress({
-        factoryAddress: FACTORY_ADDRESS[chainId],
-        tokenA: inputCurrency?.isToken ? inputCurrency : inputCurrency?.wrapped,
-        tokenB: outputCurrency?.isToken ? outputCurrency : outputCurrency?.wrapped,
-      }).toLowerCase() // pairAddress
-  :  (router.query.id as string).toLowerCase() // router string
+      factoryAddress: FACTORY_ADDRESS[chainId],
+      tokenA: inputCurrency?.isToken ? inputCurrency : inputCurrency?.wrapped,
+      tokenB: outputCurrency?.isToken ? outputCurrency : outputCurrency?.wrapped,
+    }).toLowerCase() // pairAddress
+    : (router.query.id as string).toLowerCase() // router string
 
   // const id = pairAddress ?? (router.query.id as string).toLowerCase()
 
@@ -118,6 +118,15 @@ export default function Pair({ inputCurrency, outputCurrency }: PairProps) {
   // for links
   const token0Address = currency0?.isToken ? currency0.address : currency0?.wrapped.address
   const token1Address = currency1?.isToken ? currency1.address : currency1?.wrapped.address
+  const zeroIsETH = currency0?.wrapped.address == WNATIVE_ADDRESS[chainId]
+  const oneIsETH = currency1?.wrapped.address == WNATIVE_ADDRESS[chainId]
+  const containsETH = zeroIsETH || oneIsETH
+  const PAIR_URL = 
+    containsETH && zeroIsETH
+    ? `${NATIVE[chainId].symbol}/${token1Address}` 
+      : containsETH && oneIsETH
+    ? `${NATIVE[chainId].symbol}/${token0Address}` 
+      : `${token0Address}/${token1Address}`
 
   // for info cards
   const liquidityUSDChange = pair?.reserveUSD / pair1d?.reserveUSD
@@ -140,7 +149,7 @@ export default function Pair({ inputCurrency, outputCurrency }: PairProps) {
 
   return (
     <AnalyticsContainer>
-            <div className="relative h-8">
+      <div className="relative h-8">
         <div className="absolute w-full h-full bg-gradient-to-r from-blue to-purple opacity-5" />
         <div className="absolute flex items-center w-full p-2 lg:pl-14">
           <div className="text-xs font-medium text-secondary m-1">
@@ -159,21 +168,19 @@ export default function Pair({ inputCurrency, outputCurrency }: PairProps) {
       </div>
       <TridentHeader className="sm:!flex-row justify-between items-center" pattern="bg-bubble">
         <div className="space-y-4">
-            <div className="flex items-center mt-2 space-x-4">
-              <DoubleCurrencyLogo
-                // className="-space-x-3"
-                // logoClassName="rounded-full"
-                currency0={currency0}
-                currency1={currency1}
-                size={54}
-              />
-              <Typography variant="h2" className="text-high-emphesis" weight={700}>
-                {pair?.token0?.symbol}-{pair?.token1?.symbol}
-              </Typography>
-            </div>
-            {/* <Typography variant="sm" weight={400}>
+          <div className="flex items-center mt-2 space-x-4">
+            <DoubleCurrencyLogo
+              currency0={currency0}
+              currency1={currency1}
+              size={54}
+            />
+            <Typography variant="h2" className="text-high-emphesis" weight={700}>
+              {pair?.token0?.symbol}-{pair?.token1?.symbol}
+            </Typography>
+          </div>
+          <Typography variant="sm" weight={400}>
               Dive deeper in the analytics of the {currency0.symbol}-{currency1.symbol} liquidity pool.
-            </Typography> */}
+            </Typography>
         </div>
       </TridentHeader>
       <div className="px-4 pt-4 space-y-4 lg:px-14">
@@ -207,19 +214,19 @@ export default function Pair({ inputCurrency, outputCurrency }: PairProps) {
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
           {times(2).map((i) => (
             <div key={i} className="w-full p-6 space-y-2 border rounded bg-dark-1000 border-dark-700">
-                <NavLink href={`/analytics/tokens/${[token0Address, token1Address][i]}`}>
-              <Button
-                className={`w-full hover:border hover:border-purple`}
+              <NavLink href={`/analytics/tokens/${[token0Address, token1Address][i]}`}>
+                <Button
+                  className={`w-full hover:border hover:border-purple`}
                 // variant="filled"
                 // color={getChainColorCode(chainId)}
-              >
-              <div className="flex flex-row items-center space-x-2">
-                  <CurrencyLogo size={32} currency={[currency0, currency1][i]} />
-                <div className="text-2xl text-center font-bold">{formatNumber([pair?.reserve0, pair?.reserve1][i])}</div>
-                <div className="text-xl m-1 text-white font-bold">{[pair?.token0, pair?.token1][i]?.symbol}</div>
-              </div>
-              </Button>
-                </NavLink>
+                >
+                  <div className="flex flex-row items-center space-x-2">
+                    <CurrencyLogo size={32} currency={[currency0, currency1][i]} />
+                    <div className="text-2xl text-center font-bold">{formatNumber([pair?.reserve0, pair?.reserve1][i])}</div>
+                    <div className="text-xl m-1 text-white font-bold">{[pair?.token0, pair?.token1][i]?.symbol}</div>
+                  </div>
+                </Button>
+              </NavLink>
               <div className="font-bold">
                 1 {[pair?.token0, pair?.token1][i]?.symbol} = {formatNumber([pair?.token1Price, pair?.token0Price][i])}{' '}
                 {[pair?.token1, pair?.token0][i]?.symbol} (
@@ -243,7 +250,18 @@ export default function Pair({ inputCurrency, outputCurrency }: PairProps) {
             percent={utilisation1dChange}
           />
         </div>
-        <div className="text-2xl font-bold text-high-emphesis">Information</div>
+        {/* <div className="text-3xl font-bold text-high-emphesis">Information</div> */}
+        <NavLink
+          href={`/add/${PAIR_URL}`}
+        >
+          <Button
+            size="xs"
+            variant="filled"
+            color={getChainColorCode(chainId)}
+          >
+            {`ADD LIQUIDITY`}
+          </Button>
+        </NavLink>
         <div>
           <div className="px-4 text-sm leading-48px text-high-emphesis">
             <table className="w-full table-fixed">
