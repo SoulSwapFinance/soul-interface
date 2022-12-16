b44d4fee58bb573aa1fc646556933e62067b88f7
Buns <bunsthedev@gmail.com>
2022-12-16 13:10:13 -0600

    Revert "add: NFT component (wip)"
    This reverts commit 6101c1e92087292f7044888a65923902c634b3c6.

--- src/features/nft/lib/fetcher.ts
+++
@@ -1,9 +0,0 @@
-/**
- * swr's fetcher function. [Reference](https://swr.vercel.app/docs/data-fetching#fetch)
- * @param href An url to be fetched
- * @returns The API's response
- */
-export default async function fetcher(href: string) {
-  const res = await fetch(href)
-  return res.json()
-}
--- src/features/nft/lib/types.d.ts
+++
@@ -1,3 +0,0 @@
-type ChainId = 1 | 250
-
-type Overwrite<T, U> = Pick<T, Exclude<keyof T, keyof U>> & U
