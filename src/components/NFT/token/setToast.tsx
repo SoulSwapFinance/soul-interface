aff400fa9b8e89dcd7296899e6ca92beaf42f65a
Buns <bunsthedev@gmail.com>
2022-12-16 13:09:59 -0600

    Revert "update: NFT components"
    This reverts commit 4063f701d4214396d2d35092192a5f698e9fa806.

--- src/components/NFT/token/setToast.tsx
+++
@@ -1,7 +0,0 @@
-import Toast from 'components/NFT/Toast'
-import { ComponentProps } from 'react'
-import toast from 'react-hot-toast'
-
-export const setToast: (data: ComponentProps<typeof Toast>['data']) => any = (
-  data
-) => toast.custom((t) => <Toast t={t} toast={toast} data={data} />)
