6b806fd4c19f2b6392594d5efca7a94be3c144ff
Buns <68980965+BunsDev@users.noreply.github.com>
2023-01-22 19:47:47 -0600

    try-fix: stake top margin

--- src/pages/soul/autostake/index.tsx
+++ src/pages/soul/autostake/index.tsx
@@ -174,7 +174,7 @@ export default function AutoStake() {
   }
 
   return (
-    <Container id="autostake-page" className={`md:py-8 lg:py-12 p-1 rounded rounded-2xl border border-4 border-[${getChainColor(chainId)}]`}>
+    <Container id="autostake-page" className={`md:py-8 lg:py-12 p-1 mt-4 rounded rounded-2xl border border-4 border-[${getChainColor(chainId)}]`}>
       <Head>
         <title>AutoStake | Soul</title>
         <meta key="description" name="description" />
