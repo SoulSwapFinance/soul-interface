65d2ed5b1a8ee222d10986c1a73df4b6bc8bf328
Buns <68980965+BunsDev@users.noreply.github.com>
2023-02-06 02:31:53 -0600

    update: multipliers

--- src/sdk/constants/index.ts
+++ src/sdk/constants/index.ts
@@ -63,20 +63,22 @@ export const defaultSwapFee = _25
 export const defaultProtocolFeeDenominator = FIVE
 
 
-const DAI_FTM_MARKET = '0xF4Bfdd73FE65D1B46b9968A24443A77ab89908dd'
-const DAI_BNB_MARKET = '0xbDa9204e6D596feCf9bd48108723F9BDAa2019f6'
-const FTM_DAI_MARKET = '0xFD9BE6a83c7e9cFF48f6D9a3036bb6b20598ED61'
-const DAI_ETH_MARKET = '0x9fA5de19495331E13b443F787B90CdD22B32263d'
-// const DAI_BTC_MARKET = '0xaf28730165634A56434ca7f0B302CC54F862046F'
+const DAI_FTM_MARKET = '0xF4Bfdd73FE65D1B46b9968A24443A77ab89908dd' // 7
+const DAI_BNB_MARKET = '0xbDa9204e6D596feCf9bd48108723F9BDAa2019f6' // 6
+const FTM_DAI_MARKET = '0xFD9BE6a83c7e9cFF48f6D9a3036bb6b20598ED61' // 6
+const DAI_ETH_MARKET = '0x9fA5de19495331E13b443F787B90CdD22B32263d' // 3
+// const DAI_BTC_MARKET = '0xaf28730165634A56434ca7f0B302CC54F862046F' // 1
 
 
 export const LEND_MULTIPLIER = (chainId: ChainId, pairAddress: string) => {
   let defaultMultiplier = 1
   if (chainId == ChainId.FANTOM) {
-    pairAddress == DAI_FTM_MARKET || pairAddress == DAI_BNB_MARKET || pairAddress == FTM_DAI_MARKET
-          ? defaultMultiplier = 4
+    pairAddress == DAI_FTM_MARKET 
+    ? defaultMultiplier = 7
+      : pairAddress == DAI_BNB_MARKET || pairAddress == FTM_DAI_MARKET
+          ? defaultMultiplier = 6
             : pairAddress == DAI_ETH_MARKET
-              ? defaultMultiplier = 2
+              ? defaultMultiplier = 3
                 : defaultMultiplier = 1
     return defaultMultiplier
   } else return 1
