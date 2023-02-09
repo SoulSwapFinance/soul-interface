842a53c9f8a966d5e41fc9f8cf48d7ae92007f72
Buns <68980965+BunsDev@users.noreply.github.com>
2023-02-09 14:54:40 -0600

    fix: multiplier

--- src/sdk/constants/index.ts
+++ src/sdk/constants/index.ts
@@ -66,7 +66,7 @@ export const LEND_MULTIPLIER = (chainId: ChainId, pairAddress: string) => {
   let defaultMultiplier = 1
   if (chainId == ChainId.FANTOM) {
     pairAddress == DAI_NATIVE_MARKET[250]
-    ? defaultMultiplier = 7
+    ? defaultMultiplier = 7 
       : pairAddress == DAI_BNB_MARKET[250] || pairAddress == NATIVE_DAI_MARKET[250]
           ? defaultMultiplier = 6
             : pairAddress == DAI_ETH_MARKET[250]
