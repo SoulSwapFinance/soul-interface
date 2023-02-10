1884de7fe26c6a4390ecf3981d7c0a315aeb81da
Buns <bunsthedev@gmail.com>
2023-02-09 19:47:43 -0600

    add: Donate Banner

--- src/pages/exchange/aggregator/[[...tokens]].tsx
+++ src/pages/exchange/aggregator/[[...tokens]].tsx
@@ -44,7 +44,7 @@ import { i18n } from '@lingui/core'
 import { t } from '@lingui/macro'
 import Image from 'next/image'
 import META_BANNER from 'assets/branding/meta-banner.png'
-import { MainBanner } from 'components/Banner'
+import { DonateBanner } from 'components/Banner'
 import TokenStats from 'components/TokenStats'
 
 /*
@@ -426,7 +426,7 @@ const Aggregator = ({ }) => {
 		<DoubleGlowShadowV2>
           <div className={`grid p-1 mt-4 space-y-2 rounded rounded-2xl bg-dark-1000`}>
 				{/* <SwapLayoutCard> */}
-				<MainBanner chainId={chainId} />
+				<DonateBanner chainId={chainId} />
 				<div
               className={`w-full grid grid-cols-2 p-4 rounded rounded-2xl border border-2 border-purple`}
             >
