5b39767b5a484970f46a1b22ce5370386b95de6e
Buns <bunsthedev@gmail.com>
2023-03-18 23:17:27 -0500

    update: Banner

--- src/pages/exchange/add/[[...tokens]].tsx
+++ src/pages/exchange/add/[[...tokens]].tsx
@@ -51,7 +51,7 @@ import { getChainColor, getChainColorCode } from 'constants/chains'
 // import { SwapLayoutCard } from 'layouts/SwapLayout'
 // import { SubmitButton } from 'features/summoner/Styles'
 // import SoulLogo from 'components/SoulLogo'
-import { DonateBanner, LuxorBanner } from 'components/Banner'
+import { DeFarmBanner, DonateBanner, LuxorBanner } from 'components/Banner'
 
 import Image from 'next/image'
 import ADD_BANNER from 'assets/branding/add-banner.png'
@@ -370,7 +370,7 @@ export default function Add() {
       <div className={`grid p-1 mt-8 space-y-2 rounded rounded-2xl bg-dark-1000`}>
           {/* <SwapLayoutCard> */}
           {/* <DonateBanner chainId={chainId} /> */}
-          <LuxorBanner chainId={chainId} />
+          <DeFarmBanner chainId={chainId} />
           <div
               className={`w-full grid grid-cols-2 p-4 rounded rounded-2xl border border-2 border-purple`}
             >
--- src/pages/exchange/remove/[[...tokens]].tsx
+++ src/pages/exchange/remove/[[...tokens]].tsx
@@ -50,7 +50,7 @@ import { classNames } from 'functions'
 import { getChainColor, getChainColorCode } from 'constants/chains'
 import SwapDropdown from 'features/swap/SwapDropdown'
 // import { SwapLayoutCard } from 'layouts/SwapLayout'
-import { DonateBanner, LuxorBanner } from 'components/Banner'
+import { DonateBanner, DeFarmBanner } from 'components/Banner'
 import Image from 'next/image'
 import REMOVE_BANNER from 'assets/branding/remove-banner.png'
 import TokenStats from 'components/TokenStats'
@@ -722,7 +722,7 @@ export default function Remove() {
       <div className={`grid p-1 mt-8 space-y-2 rounded rounded-2xl bg-dark-1000`}>
           {/* <SwapLayoutCard> */}
           {/* <DonateBanner chainId={chainId} /> */}
-          <LuxorBanner chainId={chainId} />
+          <DeFarmBanner chainId={chainId} />
 
           <div
               className={`w-full grid grid-cols-2 p-4 rounded rounded-2xl border border-2 border-purple`}
