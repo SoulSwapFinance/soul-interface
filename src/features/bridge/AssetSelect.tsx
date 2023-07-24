d7c0cd97d64651f6d86564bcc635c9575877b428
Buns <bunsthedev@gmail.com>
2023-07-24 00:58:11 -0500

    fix: bridge AssetSelect

--- src/features/bridge/AssetSelect.tsx
+++ src/features/bridge/AssetSelect.tsx
@@ -2,13 +2,13 @@ import { ArrowDownIcon, ArrowRightIcon } from '@heroicons/react/24/outline'
 import Container from '../../components/Container'
 import Head from 'next/head'
 import Link from 'next/link'
-import Typography from '../../components/Typography'
+// import Typography from '../../components/Typography'
 import React, { useMemo } from 'react'
 // import NetworkGuard from 'guards/Network'
-import { Feature } from 'enums/Feature'
-import { classNames, featureEnabled } from 'functions'
-import { ChainId } from 'sdk'
-import { useActiveWeb3React } from 'services/web3'
+// import { Feature } from 'enums/Feature'
+// import { classNames, featureEnabled } from 'functions'
+// import { ChainId } from 'sdk'
+// import { useActiveWeb3React } from 'services/web3'
 import { useRouter } from 'next/router'
 
 const SOUL = () => [
@@ -35,7 +35,6 @@ const SOUL = () => [
 export default function AssetSelect() {
     // const { chainId } = useActiveWeb3React()
     const asset = useMemo(() => SOUL(), [])
-    const router = useRouter()
     const { asPath } = useRouter()
     const startPage = asPath == ('/exchange/bridge') || asPath == ('/bridge')
 
@@ -58,8 +57,8 @@ export default function AssetSelect() {
                     </div>
                 </div>
             </div>
-            {asset.map((asset) => (
-                <ul className="space-y-4 divide-y-0">
+            <ul className="space-y-4 divide-y-0">
+                {asset.map((asset) => (
                     <li key={asset.id} className={`relative border gap-4 ${asPath.startsWith(asset.href) ? `border-purple hover:border-dark-600` : ''} w-full p-4 rounded bg-dark-900 hover:bg-dark-800`}>
                         <div className="flex justify-between space-y-4 space-x-4">
                             <div className="flex-1 min-w-0">
@@ -76,8 +75,8 @@ export default function AssetSelect() {
                             </div>
                         </div>
                     </li>
-                </ul>
-            ))}
+                ))}
+            </ul>
         </Container>
     )
 }
