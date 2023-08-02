375bf60fdc97e7f603b03277fe60cf9b0520f9cb
Buns Enchantress <bunsthedev@gmail.com>
2023-08-02 15:33:29 -0400

    fix: default header error

--- src/features/limit/LimitHeader.tsx
+++ src/features/limit/LimitHeader.tsx
@@ -6,10 +6,13 @@ import { classNames } from 'functions'
 import { getChainColorCode } from 'constants/chains'
 import Typography from 'components/Typography'
 import { useRouter } from 'next/router'
+import { ChainId, NATIVE, SOUL_ADDRESS, USDC_ADDRESS } from 'sdk'
 
-export default function LimitHeader({ input, output }): JSX.Element {
+export default function LimitHeader({ inputCurrency, outputCurrency }): JSX.Element {
   const { chainId } = useActiveWeb3React()
   const router = useRouter()
+  const soulEnabled = [ChainId.FANTOM, ChainId.AVALANCHE, ChainId.ETHEREUM].includes(chainId)
+
   const isExchange = router.pathname.startsWith('/swap')
     || (router.pathname.startsWith('/exchange/swap') && !router.pathname.startsWith('/exchange/swap/limit'))
   
@@ -21,7 +24,12 @@ export default function LimitHeader({ input, output }): JSX.Element {
       <NavLink
         className={isExchange ? classNames("font-bold text-high-emphesis", `bg-${getChainColorCode(chainId)} rounded-2xl`) : ''}
         // activeClassName={classNames("font-bold text-high-emphesis rounded-2xl", `bg-${getChainColorCode(chainId)}`)}
-        href={`/exchange/swap?inputCurrency=${currencyId(input)}&outputCurrency=${currencyId(output)}`}
+        // href={`/exchange/swap?inputCurrency=${input ? currencyId(input) : NATIVE[chainId].symbol}&outputCurrency=${output ? currencyId(output) : SOUL_ADDRESS[chainId]}`}
+        href={
+          inputCurrency && outputCurrency ?
+            `/exchange/swap?inputCurrency=${currencyId(inputCurrency)}&outputCurrency=${currencyId(outputCurrency)}`
+            : `/exchange/swap?inputCurrency=${NATIVE[chainId].symbol}&outputCurrency=${soulEnabled ? SOUL_ADDRESS[chainId] : USDC_ADDRESS[chainId]}`
+        }
         // href={`/exchange/swap?inputCurrency=${currencyId(input)}&outputCurrency=${currencyId(output)}`)}
       >
         <Typography
@@ -32,11 +40,15 @@ export default function LimitHeader({ input, output }): JSX.Element {
       </NavLink>
       <NavLink
         onClick={(event) => {
-          if (!output) event.preventDefault()
+          if (!outputCurrency) event.preventDefault()
         }}
         className={isLimit ? classNames("font-bold text-high-emphesis", `bg-${getChainColorCode(chainId)} rounded-2xl`) : ''}
         // activeClassName={classNames("text-high-emphesis font-bold rounded-2xl", `bg-${getChainColorCode(chainId)} rounded-2xl`)}
-        href={`/exchange/swap/limit/${currencyId(input)}/${currencyId(output)}`}
+        href={
+          inputCurrency && outputCurrency ?
+          `/exchange/swap/limit/${currencyId(inputCurrency)}/${currencyId(outputCurrency)}`
+          : `/exchange/swap/limit/${NATIVE[chainId].symbol}/${soulEnabled ? SOUL_ADDRESS[chainId] : USDC_ADDRESS[chainId]}`
+      }
       >
         <Typography
           className="flex text-white items-center justify-center px-1 py-1 text-base font-medium text-center rounded-md md:px-10 hover:text-high-emphesis"
